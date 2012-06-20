0aec12e22fbbf0f4bdd49945f1999cf77b3c3928
Derek Lockhart <lockhart@csl.cornell.edu>
2012-06-20 18:48:50 -0400

    [rtler] updated to instantiate wires

--- vgen/rtler_vbase.py
+++ vgen/rtler_vbase.py
@@ -1,10 +1,16 @@
-class TempPort(object):
-  def __init__(self, type, width, name):
+
+class VerilogPort(object):
+
+  def __init__(self, type=None, width=None, name=None, str=None):
     self.type  = type
     self.width = width
     self.name  = name
+    if str:
+      self.type, self.width, self.name  = self.parse( str )
+
   def __repr__(self):
     return "Port(%s, %s, %s)" % (self.type, self.width, self.name)
+
   def __str__(self):
     if isinstance(self.width, str):
       return "%s %s %s" % (self.type, self.width, self.name)
@@ -13,14 +19,6 @@ class TempPort(object):
         return "%s %s" % (self.type, self.name)
       else :
         return "%s [%d:0] %s" % (self.type, self.width-1, self.name)
-
-class VerilogPort(object):
-
-  def __init__(self, line):
-    self.type, self.width, self.name = self.parse(line)
-
-  def __repr__(self):
-    return "Port(%s, %s, %s)" % (self.type, self.width, self.name)
 
   def parse(self, line):
     tokens = line.strip().strip(',').split()
@@ -32,6 +30,25 @@ class VerilogPort(object):
       name = tokens[2]
       width = tokens[1]
     return type, width, name
+
+
+class VerilogWire(object):
+
+  def __init__(self, name, width):
+    self.name  = name
+    self.width = width
+
+  def __repr__(self):
+    return "Wire(%s, %s)" % (self.name, self.width)
+
+  def __str__(self):
+    if isinstance(self.width, str):
+      return "wire %s %s;" % (self.width, self.name)
+    elif isinstance(self.width, int):
+      if self.width == 1:
+        return "wire %s;" % (self.name)
+      else :
+        return "wire [%d:0] %s;" % (self.width-1, self.name)
 
 
 class VerilogParam(object):
@@ -76,34 +93,38 @@ class FromVerilog(object):
       # Find the beginning of the module definition
       if not in_module and line.startswith( start_token ):
         in_module = True
-        self.name = line.split()[1]
+        self.type = line.split()[1]
+        self.name = None
       # Parse parameters
       if in_module and 'parameter' in line:
         self.params += [ VerilogParam( line ) ]
       # Parse inputs
       elif in_module and 'input' in line:
-        self.ports += [ VerilogPort( line ) ]
+        self.ports += [ VerilogPort( str=line ) ]
       # Parse outputs
       elif in_module and 'output' in line:
-        self.ports += [ VerilogPort( line ) ]
+        self.ports += [ VerilogPort( str=line ) ]
       # End module definition
       elif in_module and end_token in line:
         in_module = False
+
 
 class ToVerilog(object):
 
   def generate(self, o):
     print >> o, 'module %s' % self.name
     # Declare Params
-    #if self.params: self.gen_params( self.params, o )
+    #if self.params: self.gen_param_decls( self.params, o )
     # Declare Ports
-    if self.ports: self.gen_ports( self.ports, o )
+    if self.ports: self.gen_port_decls( self.ports, o )
     # Wires & Instantiations
+    if self.wires: self.gen_wire_decls( self.wires, o )
+    if self.submodules: self.gen_module_insts( self.submodules, o )
     #if logic:  print logic.getvalue(),
     # End module
     print >> o, 'endmodule'
 
-  def gen_ports(self, ports, o):
+  def gen_port_decls(self, ports, o):
     print >> o, '('
     for p in ports[:-1]:
       print >> o , '  %s,' % p
@@ -111,7 +132,7 @@ class ToVerilog(object):
     print >> o, '  %s' % p
     print >> o, ');'
 
-  def gen_params(params, o):
+  def gen_param_decls(self, params, o):
     print >> o, '#('
     for p in params[:-1]:
       print >> o, '  %s,' % p
@@ -119,7 +140,23 @@ class ToVerilog(object):
     print >> o, '  %s' % p
     print >> o, ')'
 
+  def gen_wire_decls(self, wires, o):
+    for w in wires.values():
+      print >> o, '  %s' % w
+
+  def gen_module_insts(self, submodules, o):
+    for s in submodules:
+      print >> o, '  %s %s' % (s.type, s.name)
+      # TODO: add params
+      print >> o, '  ('
+      self.gen_port_insts(s.ports, o)
+      print >> o, '  );'
+
+  def gen_port_insts(self, ports, o):
+    for p in ports[:-1]:
+      print >> o , '    .%s (%s),' % (p.name, p.connection)
+    p = ports[-1]
+    print >> o, '    .%s (%s)' % (p.name, p.connection)
+
 
 req_resp_port = FromVerilog("vgen-TestMemReqRespPort.v")
-print req_resp_port
-print req_resp_port.clk
--- vgen/rtler_vbase.t.py
+++ vgen/rtler_vbase.t.py
@@ -5,47 +5,112 @@ from rtler_vbase import *
 class MagicMemory(ToVerilog):
 
   def __init__(self, num_ports, addr_sz, data_sz):
+    self.wires = {}
     # Set module name
     self.name = 'MagicMemory'
     # Define ports
     self.ports = [
-        TempPort('input', 1, 'clk'),
-        TempPort('input', 1, 'reset'),
+        VerilogPort('input', 1, 'clk'),
+        VerilogPort('input', 1, 'reset'),
     ]
     for i in range(num_ports):
       # TODO: calculate correctly!
       req_sz = addr_sz + data_sz
       rsp_sz = addr_sz + data_sz
       self.ports += [
-          TempPort('input',  req_sz, 'memreq_msg%d'%i),
-          TempPort('input',       1, 'memreq_val%d'%i),
-          TempPort('output',      1, 'memreq_rdy%d'%i),
-          TempPort('output', rsp_sz, 'memrsp_msg%d'%i),
-          TempPort('output',      1, 'memrsp_val%d'%i),
-          TempPort('input',       1, 'memrsp_rdy%d'%i),
+          VerilogPort('input',  req_sz, 'memreq_msg%d'%i),
+          VerilogPort('input',       1, 'memreq_val%d'%i),
+          VerilogPort('output',      1, 'memreq_rdy%d'%i),
+          VerilogPort('output', rsp_sz, 'memrsp_msg%d'%i),
+          VerilogPort('output',      1, 'memrsp_val%d'%i),
+          VerilogPort('input',       1, 'memrsp_rdy%d'%i),
       ]
     for port in self.ports:
       self.__dict__[port.name] = port
+    # Define modules
+    self.submodules = []
+    for i in range(num_ports):
+      req_resp_port = FromVerilog("vgen-TestMemReqRespPort.v")
+      # TODO: need different module name and instance name fields...
+      req_resp_port.name = 'req_resp%d' % i
+      self.submodules += [req_resp_port]
+    self.wire_submodules( num_ports )
+
+  def wire_submodules(self, num_ports):
+    for i in range(num_ports):
+      # TODO: won't work with multiple types of submodules...
+      req_resp_port = self.submodules[i]
+      req_resp_port.clk.connection   = self.clk.name
+      req_resp_port.reset.connection = self.reset.name
+      # Request
+      req_resp_port.memreq_msg.connection = \
+        self.__dict__['memreq_msg%d'%i].name
+      req_resp_port.memreq_val.connection = \
+        self.__dict__['memreq_val%d'%i].name
+      req_resp_port.memreq_rdy.connection = \
+        self.__dict__['memreq_rdy%d'%i].name
+      # Response - TODO: make resp/rsp consistent
+      req_resp_port.memresp_msg.connection = \
+        self.__dict__['memrsp_msg%d'%i].name
+      req_resp_port.memresp_val.connection = \
+        self.__dict__['memrsp_val%d'%i].name
+      req_resp_port.memresp_rdy.connection = \
+        self.__dict__['memrsp_rdy%d'%i].name
+      self.connect (
+        req_resp_port.physical_block_addr_M,
+        req_resp_port.physical_block_addr_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.read_block_M,
+        req_resp_port.read_block_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.write_en_M,
+        req_resp_port.write_en_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.memreq_msg_len_modified_M,
+        req_resp_port.memreq_msg_len_modified_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.block_offset_M,
+        req_resp_port.block_offset_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.memreq_msg_data_modified_M,
+        req_resp_port.memreq_msg_data_modified_M.name+str(i)
+        )
+      self.connect (
+        req_resp_port.arb_amo_en,
+        req_resp_port.arb_amo_en.name+str(i)
+        )
+      self.connect (
+        req_resp_port.amo_grant,
+        req_resp_port.amo_grant.name+str(i)
+        )
+
+
+  # TODO: add to verilog module baseclass? make shortcut?
+  def connect(self, port, name):
+    self.wires[name] = VerilogWire(name, port.width)
+    port.connection = name
+
+import unittest
+class TestMagicMemory(unittest.TestCase):
+
+  def test_one(self):
+    mem_2_16_8 = MagicMemory(2,16,8)
+    output = open('test.v', 'w+')
+    mem_2_16_8.generate( output )
+    output.close()
+    import os
+    cmd = 'iverilog -g2005 -Wall -I ../vc vgen-TestMemReqRespPort.v test.v'
+    x = os.system(cmd)
+    print cmd, x
+    self.assertEqual( x, 0 )
 
 
 
-mem_1_8_8 = MagicMemory(1,8,8)
-#print mem_1_8_8
-#print mem_1_8_8.ports
-
-mem_2_16_8 = MagicMemory(2,16,8)
-#print mem_2_16_8
-#print mem_2_16_8.ports
-
-mem_8_32_32 = MagicMemory(8,32,32)
-print mem_8_32_32
-print mem_8_32_32.ports
-print mem_8_32_32.memrsp_rdy7
 #import sys
 #output = sys.stdout
-#import StringIO
-#output = StringIO.StringIO()
-fd = open('test.v', 'w')
-output = fd
-mem_8_32_32.generate( output )
-
+unittest.main()
