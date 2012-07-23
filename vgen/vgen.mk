9c02dec1500fb5bc6c6f483ef5c24541119c6f35
Derek Lockhart <lockhart@csl.cornell.edu>
2012-07-23 17:26:11 -0400

    [rtler] moved active dev to rtler dir, old stuff still in vgen

--- vgen/vgen.mk
+++ vgen/vgen.mk
@@ -6,19 +6,13 @@ vgen_deps =
 
 vgen_srcs = \
   vgen-TestMemReqRespPort.v \
-  rtler-Adder.v \
-
-  #vgen-TestDualPortMem.v \
-  #vgen-TestQuadPortMem.v \
-  #vgen-QuadGen.v \
+  vgen-TestQuadPortMem.v \
+  vgen-QuadGen.v \
 
 vgen_test_srcs = \
 	vgen-TestMemReqRespPort.t.v \
-  rtler-Adder.t.v \
-
-  #vgen-TestDualPortMem.t.v \
-  #vgen-TestQuadPortMem.t.v \
-  #vgen-QuadGen.t.v \
+  vgen-TestQuadPortMem.t.v \
+  vgen-QuadGen.t.v \
 
 vgen_prog_srcs = \
 
