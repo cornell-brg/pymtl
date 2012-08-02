0752c5e073e48cfa5c60963ec83f0e917fb0b455
Derek Lockhart <lockhart@csl.cornell.edu>
2012-08-02 12:32:51 -0400

    [pymtl] added print_ast utility function to debug

--- pymtl/pymtl_debug.py
+++ pymtl/pymtl_debug.py
@@ -1,4 +1,5 @@
 import pymtl_model
+import ast, _ast
 
 def port_walk(tgt, spaces=0):
   for x in tgt.ports:
@@ -23,3 +24,63 @@ def port_walk(tgt, spaces=0):
   for x in tgt.submodules:
     print spaces*' ', x.name
     port_walk(x, spaces+3)
+
+def print_ast(ast_tree):
+  """Debug utility which prints the provided AST tree."""
+  print "="*35, "BEGIN AST", "="*35
+  PrintVisitor().visit( ast_tree )
+  print "="*35, " END AST ", "="*35
+
+class PrintVisitor(ast.NodeVisitor):
+
+  """AST Visitor class used by print_ast()."""
+
+  def __init__(self):
+    self.indent = 0
+
+  def generic_visit(self, node):
+
+    if isinstance(node, _ast.FunctionDef):
+      print "FUNCTIONDEF:",
+    elif isinstance(node, _ast.arguments):
+      print "ARGUMENTS:  ",
+    elif isinstance(node, _ast.Assign):
+      print "ASSIGN:     ",
+    else:
+      print "            ",
+
+    print self.indent*' ', node,
+
+    if isinstance(node, _ast.Module):
+      print node.body
+    elif isinstance(node, _ast.FunctionDef):
+      print node.name, [('@'+x.id, x) for x in node.decorator_list]
+    elif isinstance(node, _ast.Name):
+      print node.id
+    elif isinstance(node, _ast.Attribute):
+      print node.attr
+    elif isinstance(node, _ast.Assign):
+      print node.targets, ' = ', node.value
+    elif isinstance(node, _ast.AugAssign):
+      print node.op
+    elif isinstance(node, _ast.Call):
+      print node.func
+    elif isinstance(node, _ast.arguments):
+      print node.args
+    else:
+      print node._attributes,
+      print
+
+
+    self.indent += 3
+
+
+    for field, value in ast.iter_fields(node):
+      if isinstance(value, list):
+        for item in value:
+          if isinstance(item, ast.AST):
+            self.visit(item)
+      elif isinstance(value, ast.AST):
+        self.visit(value)
+
+    self.indent -= 3
