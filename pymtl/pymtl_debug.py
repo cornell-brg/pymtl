1236ffe4b973aa7009a90859c1e3d6481908348c
Derek Lockhart <lockhart@csl.cornell.edu>
2012-08-03 16:58:39 -0400

    [pymtl] fixed bug in port_walk, added ability to write to file

--- pymtl/pymtl_debug.py
+++ pymtl/pymtl_debug.py
@@ -1,29 +1,30 @@
 import pymtl_model
+import sys
 import ast, _ast
 
-def port_walk(tgt, spaces=0):
-  for x in tgt.ports:
-    print spaces*' ', x.parent.name, x
+def port_walk(tgt, spaces=0, o=sys.stdout):
+  for x in tgt._ports:
+    print >> o, spaces*' ', x.parent.name, x
     for y in x.connections:
       fullname = y.name
       if y.parent:
         fullname = y.parent.name+'.'+fullname
-      print spaces*' ', '   knctn: {0} {1}'.format(type(y), fullname)
-    print spaces*' ', '   value:', x._value, #x.value
+      print >> o, spaces*' ', '   knctn: {0} {1}'.format(type(y), fullname)
+    print >> o, spaces*' ', '   value:', x._value, #x.value
     if isinstance(x._value, pymtl_model.Slice):
       # TODO: handle this case in VerilogSlice instead?
       if x._value._value:
-        print x.value
+        print >> o, x.value
       else:
-        print None
-      print (spaces+1)*' ', '   slice:', x._value._value, bin(x._value.pmask)
+        print >> o, None
+      print >> o, (spaces+1)*' ', '   slice:', x._value._value, bin(x._value.pmask)
     # TODO: handle this case in VerilogSlice instead?
     else:
-      print x.value
-  print
-  for x in tgt.submodules:
-    print spaces*' ', x.name
-    port_walk(x, spaces+3)
+      print >> o, x.value
+  print >> o
+  for x in tgt._submodules:
+    print >> o, spaces*' ', x.name
+    port_walk(x, spaces+3, o)
 
 def print_ast(ast_tree):
   """Debug utility which prints the provided AST tree."""
