4eb3fc453c88d174435a72e6c6d8e83ea7de9acc
Derek Lockhart <lockhart@csl.cornell.edu>
2012-08-21 15:13:34 -0400

    [pymtl] fixed __init__ to import user visible classes

---
+++ pymtl/__init__.py
@@ -0,0 +1,15 @@
+from model import Model, Wire, Port, InPort, OutPort
+from simulate import SimulationTool
+from translate import VerilogTranslationTool
+from visualize import VisualizationTool
+
+__all__ = ['Model',
+           'Wire',
+           'Port',
+           'InPort',
+           'OutPort',
+           'SimulationTool',
+           'VerilogTranslationTool',
+           'VisualizationTool'
+          ]
+
