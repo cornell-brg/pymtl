02605860c0a1a0861cdfc762e2cc1eeb07a481d5
Derek Lockhart <lockhart@csl.cornell.edu>
2012-08-23 19:49:44 -0400

    [pymtl] fixed __init__ to export funcs in pymtl api

--- pymtl/__init__.py
+++ pymtl/__init__.py
@@ -1,4 +1,5 @@
 from model import Model, Wire, Port, InPort, OutPort
+from model import posedge_clk, combinational, connect
 from simulate import SimulationTool
 from translate import VerilogTranslationTool
 from visualize import VisualizationTool
@@ -8,6 +9,9 @@ __all__ = ['Model',
            'Port',
            'InPort',
            'OutPort',
+           'posedge_clk',
+           'combinational',
+           'connect',
            'SimulationTool',
            'VerilogTranslationTool',
            'VisualizationTool'
