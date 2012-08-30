9765845917e7888e9e2dd42ef04b858d8fb3a3ad
xw286 <xw286@cornell.edu>
2012-08-29 20:19:21 -0400

    [pmlib] added Unsign to bit.py

--- pmlib/bit.py
+++ pmlib/bit.py
@@ -21,6 +21,20 @@ class REV( Model ):
     self.out.value = ~self.in_.value
 
 #-------------------------------------------------------------------------
+# Unsign operater
+#-------------------------------------------------------------------------
+
+class Unsign( Model ):
+
+  def __init__( self, W = 16 ):
+    self.in_ = InPort( W )
+    self.out = OutPort( W )
+
+  @combinational
+  def comb_logic( self ):
+    self.out.value = ~self.in_.value + 1
+
+#-------------------------------------------------------------------------
 # MSB operater
 #-------------------------------------------------------------------------
 
@@ -66,3 +80,79 @@ class TriCon( Model ):
 #  def comb_logic( self ):
 #    print "input connections", self.in_.connections
 #    print "output connections", self.out.connections
+
+#-------------------------------------------------------------------------
+# Concatenater
+#-------------------------------------------------------------------------
+
+class CAT( Model ):
+
+  def __init__( self, W_IN0 = 16, W_IN1 = 16 ):
+    self.in0 = InPort( W_IN0 )
+    self.in1 = InPort( W_IN1 )
+    self.out = OutPort( W_IN0 + W_IN1 )
+
+    self.shiftbits = W_IN0
+
+  @combinational
+  def comb_logic( self ):
+    self.out.value = self.in0.value | ( self.in1.value << self.shiftbits )
+
+#-------------------------------------------------------------------------
+# Signal extension
+#-------------------------------------------------------------------------
+
+  #-------------------------------------------------------------------------
+  # Extend twice
+  #-------------------------------------------------------------------------
+
+class EXT2( Model ):
+
+  def __init__( self, W = 1 ):
+    self.in_ = InPort( W )
+    self.out = OutPort( W * 2 )
+
+    self.shiftbits = W
+
+  @combinational
+  def comb_logic( self ):
+    self.out.value = self.in_.value | ( self.in_.value << self.shiftbits )
+
+  #-------------------------------------------------------------------------
+  # Extend n times
+  #-------------------------------------------------------------------------
+  # Recursive call
+  # with no for-loop allowing us to assign out with a variable length
+  #   expression, we design this kind of function using recursive call
+  #   to generate a highly structual module
+
+#class EXTN( Model ):
+#
+#  def __init__( self, W = 1, N = 3 ):
+#    self.in_ = InPort( W )
+#    self.out = OutPort( W * N )
+#
+#    # Localparameter
+#    self.n = N
+#
+#    # Sub module
+#    assert N > 2
+#    self.cat = CAT( W * (N-1), W )
+#    # Even reaching boundry, we still have to generate a sub module
+#    #   otherwise the code will be not translatable
+#    if N == 3:
+#      self.extn = EXT2( W )
+#    else:
+#      self.extn = EXTN( W, N-1 )
+#
+#    # Connections
+#    connect( self.out, self.cat.out )
+#
+#  @combinational
+#  def comb_logic( self ):
+#    self.cat.in1.value = self.in_.value
+#    self.cat.in0.value = self.extn.out.value if self.n > 2 else self.in_.value
+#    # no matter if N is greater than 2, we can't let the input float
+#    #if self.n > 2:
+#    self.extn.in_.value = self.in_.value
+
--- pmlib/bit_test.py
+++ pmlib/bit_test.py
@@ -18,17 +18,15 @@ class TestREV( unittest.TestCase ):
     self.sim = SimulationTool( self.model )
 
   def test_one( self ):
-    test_cases = [ [ 0, 15,],
-                   [ 1, 14,],
-                   [ 5, 10,],
-                   [ 7, 8,],
-                   [ 9, 6,],
-                   [ 10, 5,],
-                   [ 11, 4,],
-                   [ 12, 3,],
-                   [ 13, 2,],
-                   [ 14, 1,],
-                   [ 15, 0,],
+    test_cases = [ [ 0b0000, 0b1111,],
+                   [ 0b0001, 0b1110,],
+                   [ 0b0101, 0b1010,],
+                   [ 0b0111, 0b1000,],
+                   [ 0b1001, 0b0110,],
+                   [ 0b1010, 0b0101,],
+                   [ 0b1101, 0b0010,],
+                   [ 0b1110, 0b0001,],
+                   [ 0b1111, 0b0000,],
                  ]
 
     for test in test_cases:
@@ -46,6 +44,43 @@ class TestREV( unittest.TestCase ):
 #    self.hdl.translate( 'REV.v' )
 
 #-------------------------------------------------------------------------
+# Unsign unit test
+#-------------------------------------------------------------------------
+
+class TestUnsign( unittest.TestCase ):
+
+  def setUp( self ):
+    self.model = Unsign( 4 )
+    self.model.elaborate()
+    self.sim = SimulationTool( self.model )
+
+  def test_one( self ):
+    test_cases = [ [ 0b0000, 0b0000,],
+                   [ 0b0001, 0b1111,],
+                   [ 0b0101, 0b1011,],
+                   [ 0b0111, 0b1001,],
+                   [ 0b1001, 0b0111,],
+                   [ 0b1010, 0b0110,],
+                   [ 0b1101, 0b0011,],
+                   [ 0b1110, 0b0010,],
+                   [ 0b1111, 0b0001,],
+                 ]
+
+    for test in test_cases:
+      self.model.in_.value = test[0]
+      self.sim.cycle()
+
+      self.assertEquals( self.model.out.value, test[1] )
+
+  def test_vcd( self ):
+#    self.sim.dump_vcd( 'Unsign_test.vcd' )
+    self.test_one()
+
+  def test_translate( self ):
+    self.hdl = VerilogTranslationTool( self.model )
+#    self.hdl.translate( 'Unsign.v' )
+
+#-------------------------------------------------------------------------
 # MSB unit test
 #-------------------------------------------------------------------------
 
@@ -57,23 +92,18 @@ class TestMSB( unittest.TestCase ):
     self.sim = SimulationTool( self.model )
 
   def test_one( self ):
-    test_cases = [ [ 0, 0,],
-                   [ 1, 0,],
-                   [ 5, 0,],
-                   [ 7, 0,],
-                   [ 8, 0,],
-                   [ 9, 0,],
-                   [ 10, 0,],
-                   [ 11, 0,],
-                   [ 12, 0,],
-                   [ 13, 0,],
-                   [ 14, 0,],
-                   [ 15, 0,],
-                   [ 32767, 0,],
-                   [ 32768, 1,],
-                   [ 32770, 1,],
-                   [ 65535, 1,],
-                   [ 65536, 0,],
+    test_cases = [ [ 0x0000, 0,],
+                   [ 0x0001, 0,],
+                   [ 0x0107, 0,],
+                   [ 0x1111, 0,],
+                   [ 0x1fff, 0,],
+                   [ 0x7fff, 0,],
+                   [ 0x8000, 1,],
+                   [ 0x8001, 1,],
+                   [ 0x8111, 1,],
+                   [ 0xbeef, 1,],
+                   [ 0xffff, 1,],
+                   [ 0x10000, 0,],
                  ]
 
     for test in test_cases:
@@ -114,14 +144,14 @@ class TestDoubleCon( unittest.TestCase ):
 #
 #      self.assertEquals( self.model.out.width, 2 )
 #      self.assertEquals( self.model.out.value, test[1] )
-
-  def test_vcd( self ):
+#
+#  def test_vcd( self ):
 #    self.sim.dump_vcd( 'DoubleCon_test.vcd' )
 #    self.test_one()
-    pass
-
-  def test_translate( self ):
-    self.hdl = VerilogTranslationTool( self.model )
+#    pass
+#
+#  def test_translate( self ):
+#    self.hdl = VerilogTranslationTool( self.model )
 #    self.hdl.translate( 'DoubleCon.v' )
 
 #-------------------------------------------------------------------------
@@ -138,7 +168,7 @@ class TestTriCon( unittest.TestCase ):
   def test_one( self ):
     print "\nTriple connection test currently fails"
 #    test_cases = [ [ 0, 0,],
-#                   [ 1, 8,],
+#                   [ 1, 7,],
 #                 ]
 #
 #    for test in test_cases:
@@ -147,6 +177,114 @@ class TestTriCon( unittest.TestCase ):
 #
 #      self.assertEquals( self.model.out.width, 3 )
 #      self.assertEquals( self.model.out.value, test[1] )
+#
+#  def test_translate( self ):
+#    self.hdl = VerilogTranslationTool( self.model )
+#    self.hdl.translate( 'TriCon.v' )
+
+#-------------------------------------------------------------------------
+# Concatenater unit test
+#-------------------------------------------------------------------------
+
+class TestCAT( unittest.TestCase ):
+
+  def setUp( self ):
+    self.model = CAT( 16 )
+    self.model.elaborate()
+    self.sim = SimulationTool( self.model )
+
+  def test_one( self ):
+    test_cases = [ [ 0x0000, 0x0000, 0x00000000,],
+                   [ 0x0001, 0x0008, 0x00080001,],
+                   [ 0x5555, 0xaaaa, 0xaaaa5555,],
+                   [ 0xffff, 0x1111, 0x1111ffff,],
+                 ]
+
+    for test in test_cases:
+      self.model.in0.value = test[0]
+      self.model.in1.value = test[1]
+      self.sim.cycle()
+
+      self.assertEquals( self.model.out.width, 32 )
+      self.assertEquals( self.model.out.value, test[2] )
+
+  def test_vcd( self ):
+#    self.sim.dump_vcd( 'CAT_test.vcd' )
+#    self.test_one()
+    pass
+
+  def test_translate( self ):
+    self.hdl = VerilogTranslationTool( self.model )
+#    self.hdl.translate( 'CAT.v' )
+
+#-------------------------------------------------------------------------
+# Extend2 unit test
+#-------------------------------------------------------------------------
+
+class TestEXT2( unittest.TestCase ):
+
+  def setUp( self ):
+    self.model = EXT2( 2 )
+    self.model.elaborate()
+    self.sim = SimulationTool( self.model )
+
+  def test_one( self ):
+    test_cases = [ [ 0x0, 0x00,],
+                   [ 0x1, 0x05,],
+                   [ 0x6, 0x0a,],
+                   [ 0xf, 0x0f,],
+                 ]
+
+    for test in test_cases:
+      self.model.in_.value = test[0]
+      self.sim.cycle()
+
+      self.assertEquals( self.model.out.width, 4 )
+      self.assertEquals( self.model.out.value, test[1] )
+
+  def test_vcd( self ):
+#    self.sim.dump_vcd( 'EXT2_test.vcd' )
+#    self.test_one()
+    pass
+
+  def test_translate( self ):
+    self.hdl = VerilogTranslationTool( self.model )
+#    self.hdl.translate( 'EXT2.v' )
+
+#-------------------------------------------------------------------------
+# ExtendN unit test
+#-------------------------------------------------------------------------
+
+#class TestEXTN( unittest.TestCase ):
+#
+#  def setUp( self ):
+#    self.model = EXTN( 4, 5 )
+#    self.model.elaborate()
+#    self.sim = SimulationTool( self.model )
+#
+#  def test_one( self ):
+#    test_cases = [ [ 0x0, 0x00000,],
+#                   [ 0x1, 0x11111,],
+#                   [ 0x6, 0x66666,],
+#                   [ 0xf, 0xfffff,],
+#                 ]
+#
+#    for test in test_cases:
+#      self.model.in_.value = test[0]
+#      self.sim.cycle()
+#
+#      self.assertEquals( self.model.out.width, 20 )
+#      self.assertEquals( self.model.out.value, test[1] )
+#
+#  def test_vcd( self ):
+#    self.sim.dump_vcd( 'EXTN_test.vcd' )
+#    self.test_one()
+#    pass
+#
+#  def test_translate( self ):
+#    self.hdl = VerilogTranslationTool( self.model )
+#    self.hdl.translate( 'EXTN.v' )
+
 
 if __name__ == '__main__':
   unittest.main()
