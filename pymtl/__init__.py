9748570b0195c0b6cb93fa6c83099c0e855c7452
cbatten <cbatten@cornell.edu>
2012-09-04 08:31:25 -0400

    [pymtl] do not expose old src/sink

--- pymtl/__init__.py
+++ pymtl/__init__.py
@@ -2,7 +2,6 @@ from model import Model, Wire, Port, InPort, OutPort
 from model import posedge_clk, combinational, connect
 from simulate import SimulationTool
 from translate import VerilogTranslationTool
-from test_source import TestSource, TestSink
 from connects import connect_chain, connect_auto
 #from visualize import VisualizationTool
 
@@ -18,8 +17,6 @@ __all__ = ['Model',
            'connect_auto',
            'SimulationTool',
            'VerilogTranslationTool',
-           'TestSource',
-           'TestSink',
 #           'VisualizationTool'
           ]
 
