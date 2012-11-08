06716eb2ac19dda9be9805abcfbc6770b73ccbc2
Derek Lockhart <lockhart@csl.cornell.edu>
2012-11-08 11:02:14 -0500

    [pmlib] mistake in last commit, fixed Crossbar

--- pmlib/Crossbar.py
+++ pmlib/Crossbar.py
@@ -1,20 +1,41 @@
 #=========================================================================
 # Crossbar
 #=========================================================================
+# This class implements a parameterized "nxn" square crossbar switch.
+# conceptually a cross bar with "n" inputs & "n" outputs allows each input
+# to traverse the switch and cross over to any of the output without any
+# intermediate stages given the select control signals. We can model the
+# cross bar by having "n" output muxes based on the number of output ports
+# with the select control lines.
+#
+# Interface
+# ---------
+#
+# nbits  : bitwidth of each port
+# nports : parameter for number of ports in the square "nxn" crossbar
+# in_    : list of input ports
+# sel    : list of select control lines for each output mux
+# out    : list of output ports
 
-from pymtl import *
+from   pymtl import *
 import pmlib
+import math
 
 class Crossbar ( Model ):
 
-  @capture_args
   def __init__( self, nports, nbits ):
+
+    # Local Constant
 
     self.nports = nports
 
-    self.in_ = [ InPort  ( nbits )  for x in range( nports ) ]
-    self.out = [ OutPort ( nbits )  for x in range( nports ) ]
-    self.sel = [ InPort  ( 1 )      for x in range( nports ) ]
+    sel_nbits   = int( math.ceil( math.log( nports, 2 ) ) )
+
+    # Interface Ports
+
+    self.in_    = [ InPort  ( nbits )     for x in range( nports ) ]
+    self.out    = [ OutPort ( nbits )     for x in range( nports ) ]
+    self.sel    = [ InPort  ( sel_nbits ) for x in range( nports ) ]
 
   @combinational
   def comb_logic( self ):
@@ -27,9 +48,10 @@ class Crossbar ( Model ):
     s = ''
     for i in xrange( self.nports ):
       s += "{:04x} ".format( self.in_[i].value.uint )
+    s += "( "
     for i in xrange( self.nports ):
       s += "{:01x} ".format( self.sel[i].value.uint )
-    s += "()"
+    s += ")"
     for i in xrange( self.nports ):
       s += " {:04x}".format( self.out[i].value.uint )
     return s
