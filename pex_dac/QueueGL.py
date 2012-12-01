1b2d9dab15eecf6338efd00e6816d50d3653fb0c
Derek Lockhart <lockhart@csl.cornell.edu>
2012-12-01 01:42:06 -0500

    [pex_dac] added QueueGL cell placement ex

---
+++ pex_dac/QueueGL.py
@@ -0,0 +1,53 @@
+#=========================================================================
+# Gate Level Physical Design Example
+#=========================================================================
+
+from pymtl import *
+
+#-------------------------------------------------------------------------
+# Register
+#-------------------------------------------------------------------------
+
+class Register( Model ):
+
+  def __init__( s, cell_sz ):
+    s.cell_sz = cell_sz
+
+  def physical_elaboration( s ):
+    w, h = s.cell_sz
+    s._dim.w = w
+    s._dim.h = h
+
+#-------------------------------------------------------------------------
+# Queue
+#-------------------------------------------------------------------------
+
+class QueueGL( Model ):
+
+  def __init__( s, entries, nbits, cell_sz ):
+    s.entries = entries
+    s.nbits   = nbits
+    s.regs    = [ Register( cell_sz )
+                  for x in range( entries * nbits ) ]
+
+  def physical_elaboration( s ):
+    for i in range( s.entries ):
+      for j in range( s.nbits ):
+        reg = s.regs[ i * s.nbits + j ]
+        reg.physical_elaboration()
+        reg._dim.x = i * reg._dim.h
+        reg._dim.y = j * reg._dim.w
+    s._dim.x = s.entries * reg._dim.w
+    s._dim.y = s.nbits   * reg._dim.h
+
+
+#-------------------------------------------------------------------------
+# Physical Elaboration
+#-------------------------------------------------------------------------
+
+def test_dump():
+  model = QueueGL( 3, 16, (5,10) )
+  model.elaborate()
+  model.physical_elaboration()
+  print
+  model.dump_physical_design()
