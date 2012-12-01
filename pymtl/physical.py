066e581bedef813407fa25c5174fa33add3de6d2
Derek Lockhart <lockhart@csl.cornell.edu>
2012-12-01 01:05:23 -0500

    [pex_dac] added RingAL floorplanning ex

--- pymtl/physical.py
+++ pymtl/physical.py
@@ -11,16 +11,16 @@ class PhysicalDimensions(object):
   def __init__( self ):
     self.x = 0
     self.y = 0
-    self.h = 0
     self.w = 0
+    self.h = 0
 
   def get_rectangle( self ):
-    x, y, h, w = self.x, self.y, self.h, self.w
-    return (x, y, h, w)
+    x, y, w, h = self.x, self.y, self.w, self.h
+    return (x, y, w, h)
 
   def get_bounding_box( self ):
     # assumes origin is top left!
-    x, y, h, w = self.x, self.y, self.h, self.w
+    x, y, w, h = self.x, self.y, self.w, self.h
     tl = ( x,     y     )  # top_left
     tr = ( x + w, y     )  # top_right
     bl = ( x,     y + h )  # bottom_left
