0e7f230493bad4196b58078c2972bbcc79031cdd
Derek Lockhart <lockhart@csl.cornell.edu>
2012-12-06 16:23:14 -0500

    [pymtl] started translate/verilate script

---
+++ pymtl/verilator_sim.py
@@ -0,0 +1,14 @@
+from translate import VerilogTranslationTool
+
+
+# TODO: figure out a way to replace module instantiation with dummy
+#       model that performs translate/verilation with provided
+#       parameters and then return the Wrapped/Verilated instance
+
+def translate_class( model_class, config=None ):
+  # TODO: currently assumes construct doesn't need any arguments
+  #       need to change it to take configuration parameters
+  model_inst = model_class()
+  model_inst.elaborate()
+  verilog_file = model_inst.class_name + '.v'
+  VerilogTranslationTool( model_inst, verilog_file )
