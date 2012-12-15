38e170ab471b38b1da29c664c34f84dadbeb29e8
Derek Lockhart <lockhart@csl.cornell.edu>
2012-12-14 22:03:53 -0500

    [v2pymtl] models w params supported + Crossbar test

--- pymtl/verilator_sim.py
+++ pymtl/verilator_sim.py
@@ -14,6 +14,33 @@ def translate_class( model_class, config=None ):
   verilog_file = model_inst.class_name + '.v'
   VerilogTranslationTool( model_inst, verilog_file )
 
+def get_verilated( model_inst ):
+
+  model_inst.elaborate()
+  model_name = model_inst.class_name
+
+  # TODO: verilog_file should depend on arguments params!
+  verilog_file = model_name + '.v'
+  VerilogTranslationTool( model_inst, verilog_file )
+
+  # Verilate the module
+  v2pymtl.v2pymtl.verilog_to_pymtl( model_inst, verilog_file )
+
+  # Use some trickery to import the verilated version of the model
+  import sys
+  sys.path.append('../build')
+  __import__( 'W' + model_name )
+  imported_module = sys.modules[ 'W'+model_name ]
+
+  # Get the model class from the module, instantiate and elaborate it
+  model_class = imported_module.__dict__[ model_name ]
+  model_inst = model_class()
+  model_inst.elaborate()
+
+  return model_inst
+
 def verilate( model_class, src_file, config=None ):
+  # Translate the model to Verilog
   translate_class( model_class )
+  # Verilate the generated Verilog into Python wrapped C++
   v2pymtl.v2pymtl.verilog_to_pymtl( model_class, src_file )
