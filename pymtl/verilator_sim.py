7b0e5c69237d182ae16750843add78e4f003ee4b
Derek Lockhart <lockhart@csl.cornell.edu>
2013-01-13 11:35:32 -0500

    [pymtl] add caching of verilations for faster tests

--- pymtl/verilator_sim.py
+++ pymtl/verilator_sim.py
@@ -1,6 +1,8 @@
 from translate import VerilogTranslationTool
 import v2pymtl
 
+import os
+import filecmp
 
 # TODO: figure out a way to replace module instantiation with dummy
 #       model that performs translate/verilation with provided
@@ -19,12 +21,24 @@ def get_verilated( model_inst ):
   model_inst.elaborate()
   model_name = model_inst.class_name
 
-  # TODO: verilog_file should depend on arguments params!
+  # Translate the PyMTL module to Verilog, if we've already done
+  # translation check if there's been any changes to the source
   verilog_file = model_name + '.v'
-  VerilogTranslationTool( model_inst, verilog_file )
+  temp_file    = verilog_file + '.tmp'
+  if os.path.exists( verilog_file ):
+    VerilogTranslationTool( model_inst, temp_file )
+    cached = filecmp.cmp( temp_file, verilog_file )
+    if not cached:
+      os.system( ' diff %s %s'%( temp_file, verilog_file ))
+    os.rename( temp_file, verilog_file )
+  else:
+    VerilogTranslationTool( model_inst, verilog_file )
+    cached = False
 
-  # Verilate the module
-  v2pymtl.v2pymtl.verilog_to_pymtl( model_inst, verilog_file )
+  # Verilate the module only if we've updated the verilog source
+  if not cached:
+    print "NOT CACHED", verilog_file
+    v2pymtl.v2pymtl.verilog_to_pymtl( model_inst, verilog_file )
 
   # Use some trickery to import the verilated version of the model
   import sys
