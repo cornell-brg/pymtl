60b218576c58da9febf6a39435a7267af8accec3
Derek Lockhart <lockhart@csl.cornell.edu>
2013-03-25 01:28:42 -0400

    [new_pymtl] test for new ast visitor arch

---
+++ new_pymtl/ast_visitor.py
@@ -0,0 +1,232 @@
+#=========================================================================
+# AST Visitor
+#=========================================================================
+
+import inspect
+import ast, _ast
+
+#------------------------------------------------------------------------
+# Sensitivity List Visitor
+#------------------------------------------------------------------------
+class LeafVisitor( ast.NodeVisitor ):
+
+  def __init__( self ):
+    self.assign = False
+    self.load   = [ ]
+    self.store  = [ ]
+
+  def enter( self, node ):
+    self.visit( node )
+    return self.load, self.store
+
+  # Start here, visit each assignment statement and detect:
+  # 1) sensitivity list: all variables read
+  # 2) reg types: all variables written to
+  def visit_Assign( self, node ):
+    # We currently don't handle multiple LHS targets
+    assert len( node.targets ) == 1
+
+    self.assign = True
+    self.visit( node.targets[0] )
+    self.visit( node.value )
+    self.assign = False
+
+  def visit_Attribute( self, node ):
+    if not self.assign: return
+    if   isinstance( node.ctx, _ast.Load ):
+      self.load  += [ LeafChecker( self ).visit( node ) ]
+    elif isinstance( node.ctx, _ast.Store ):
+      self.store += [ LeafChecker( self ).visit( node ) ]
+    else:
+      print type( node.ctx )
+      raise Exception( "Unsupported concurrent block code!" )
+
+  def visit_Name( self, node ):
+    if not self.assign: return
+    if   isinstance( node.ctx, _ast.Load ):
+      self.load  += [ LeafChecker( self ).visit( node ) ]
+    elif isinstance( node.ctx, _ast.Store ):
+      self.store += [ LeafChecker( self ).visit( node ) ]
+    else:
+      print type( node.ctx )
+      raise Exception( "Unsupported concurrent block code!" )
+
+  # TODO: need this to detect writes to bit slices?
+  #def visit_Subscript( self, node ):
+  #  if not self.assign: return
+  #  if   isinstance( node.ctx, _ast.Load ):
+  #    self.load  += [ LeafChecker( self ).visit( node ) ]
+  #  elif isinstance( node.ctx, _ast.Store ):
+  #    self.store += [ LeafChecker( self ).visit( node ) ]
+  #  else:
+  #    print type( node.ctx )
+  #    raise Exception( "Unsupported concurrent block code!" )
+
+
+class LeafChecker( ast.NodeVisitor ):
+
+  def __init__( self, parent ):
+    self.parent = parent
+
+  def visit_Attribute( self, node ):
+    return self.visit( node.value ) + '.' + node.attr
+
+  def visit_Subscript( self, node ):
+    self.parent.visit( node.slice )
+    return self.visit( node.value ) + '.[]'
+
+  def visit_Name( self, node ):
+    return node.id
+
+
+#-------------------------------------------------------------------------
+# Parse Method Utility Function
+#-------------------------------------------------------------------------
+# In order to parse class methods and inner functions, we need to fix the
+# indentation whitespace or else the ast parser throws an "unexpected
+# ident" error.
+import re
+p = re.compile('( *(@|def))')
+def get_method_ast( func ):
+
+  src = inspect.getsource( func )
+  new_src = p.sub( r'\2', src )
+  tree = ast.parse( new_src )
+  return tree
+
+#-------------------------------------------------------------------------
+# AST Visitor Function Decorator
+#-------------------------------------------------------------------------
+
+def check_ast( ld, st ):
+
+  def check_decorator( func ):
+    tree = get_method_ast( func )
+
+    print func.__name__
+    #import debug_utils
+    #debug_utils.print_ast( tree )
+
+    load, store = LeafVisitor().enter( tree )
+    print "LOADS ", load,  "want:", ld
+    print "STORES", store, "want:", st
+    assert ld == load
+    assert st == store
+
+    return func
+
+  return check_decorator
+
+
+#-------------------------------------------------------------------------
+# Simple Assignments
+#-------------------------------------------------------------------------
+
+def test_assign():
+  @check_ast( ['s.in_'], ['s.out.v'] )
+  def assign( s ):
+    s.out.v = s.in_
+
+def test_assign_op():
+  @check_ast( ['s.a.v', 's.b', 's.c'], ['s.out.v'] )
+  def assign_op( s ):
+    s.out.v = s.a.v + s.b + s.c
+
+def test_assign_temp():
+  @check_ast( ['s.in_', 'x'], ['x', 's.out.v'] )
+  def assign_temp( s ):
+    x = s.in_
+    s.out.v = x
+
+#-------------------------------------------------------------------------
+# Bit Indexing
+#-------------------------------------------------------------------------
+
+def test_rd_bit_idx_const():
+  @check_ast( ['s.a.v', 's.b'], ['s.out.v'] )
+  def rd_bit_idx_const( s ):
+    s.out.v = s.a.v[ 0 ] + s.b[ 1 ]
+
+def test_rd_bit_idx_var():
+  @check_ast( ['s.a.v', 's.c', 's.b', 's.d'], ['s.out.v'] )
+  def rd_bit_idx_var( s ):
+    s.out.v = s.a.v[ s.c ] & s.b[ s.d ]
+
+def test_rd_bit_idx_slice_const():
+  @check_ast( ['s.a.v', 's.b'], ['s.out.v'] )
+  def rd_bit_idx_slice_const( s ):
+    s.out.v = s.a.v[ 0:2 ] & s.b[ 4:8 ]
+
+def test_rd_bit_idx_slice_var():
+  @check_ast( ['s.a.v', 's.s0', 's.s1'], ['s.out.v'] )
+  def rd_bit_idx_slice_var( s ):
+    s.out.v = s.a.v[ s.s0:s.s1 ]
+
+import pytest
+@pytest.mark.xfail
+def test_wr_bit_idx_const():
+  @check_ast( ['s.in0', 's.in1'], ['s.out.v'] )
+  def wr_bit_idx_const( s ):
+    s.out.v[ 0 ] = s.in0 + s.in1
+
+@pytest.mark.xfail
+def test_wr_bit_idx_var():
+  @check_ast( ['s.c', 's.in0', 's.in1'], ['s.out.v'] )
+  def wr_bit_idx_var( s ):
+    s.out.v[ s.c ] = s.in0 + s.in1
+
+@pytest.mark.xfail
+def test_wr_bit_idx_slice_const():
+  @check_ast( ['s.in0'], ['s.out.v'] )
+  def wr_bit_idx_slice_const( s ):
+    s.out.v[ 0:1 ] = s.in0[ 3:4 ]
+
+@pytest.mark.xfail
+def test_wr_bit_idx_slice_var():
+  @check_ast( ['s.a.v', 's.s0', 's.s1'], ['s.out.v'] )
+  def wr_bit_idx_slice_var( s ):
+    s.out.v[ s.s0:s.s1 ] = s.a.v
+
+#-------------------------------------------------------------------------
+# List Indexing
+#-------------------------------------------------------------------------
+
+def test_rd_list_idx_const():
+  @check_ast( ['s.a.[].v', 's.b.[].v'], ['s.out.v'] )
+  def rd_list_idx_const( s ):
+    s.out.v = s.a[ 0 ].v + s.b[ 1 ].v
+
+def test_rd_list_idx_var():
+  @check_ast( ['s.c', 's.a.[].v', 's.b', 's.d'], ['s.out.v'] )
+  def rd_list_idx_var( s ):
+    s.out.v = s.a[ s.c ].v & s.b[ s.d ]
+
+def test_rd_list_idx_slice_const():
+  @check_ast( ['s.a.[].v', 's.b.[].v'], ['s.out.v'] )
+  def rd_list_idx_slice_const( s ):
+    s.out.v = s.a[ 0:2 ].v & s.b[ 4:8 ].v
+
+def test_rd_list_idx_slice_var():
+  @check_ast( ['s.s0', 's.s1', 's.a.[].v'], ['s.out.v'] )
+  def rd_list_idx_slice_var( s ):
+    s.out.v = s.a[ s.s0:s.s1 ].v
+
+def test_wr_list_idx_const():
+  @check_ast( ['s.in0', 's.in1'], ['s.out.[].v'] )
+  def wr_list_idx_const( s ):
+    s.out[ 0 ].v = s.in0 + s.in1
+
+def test_wr_list_idx_var():
+  @check_ast( ['s.c', 's.in0', 's.in1'], ['s.out.[].v'] )
+  def wr_list_idx_var( s ):
+    s.out[ s.c ].v = s.in0 + s.in1
+
+def test_wr_list_idx_slice_const():
+  @check_ast( ['s.in0'], ['s.out.[].v'] )
+  def wr_list_idx_slice_const( s ):
+    s.out[ 0:1 ].v = s.in0[ 3:4 ]
+
+def test_wr_list_idx_slice_var():
+  @check_ast( ['s.s0', 's.s1', 's.a.v'], ['s.out.[].v'] )
+  def wr_list_idx_slice_var( s ):
+    s.out[ s.s0:s.s1 ].v = s.a.v
