22cfba6a1c3fc2e8d275a30e0e452672e94d4c45
Derek Lockhart <lockhart@csl.cornell.edu>
2013-03-31 17:16:10 -0400

    [new_pymtl] add sim support for seq logic + tests

---
+++ new_pymtl/SimulationTool_seq_test.py
@@ -0,0 +1,62 @@
+from Model          import *
+from SimulationTool import *
+
+#-------------------------------------------------------------------------
+# Setup Sim
+#-------------------------------------------------------------------------
+
+def setup_sim( model ):
+  model.elaborate()
+  sim = SimulationTool( model )
+  return sim
+
+#-------------------------------------------------------------------------
+# Register Tester
+#-------------------------------------------------------------------------
+
+def register_tester( model_type ):
+  model = model_type( 16 )
+  sim = setup_sim( model )
+  model.in_.v = 8
+  assert model.out == 0
+  sim.cycle()
+  assert model.out == 8
+  model.in_.v = 9
+  assert model.out == 8
+  model.in_.v = 10
+  sim.cycle()
+  assert model.out == 10
+
+#-------------------------------------------------------------------------
+# RegisterOld
+#-------------------------------------------------------------------------
+
+class RegisterOld( Model ):
+  def __init__( s, nbits ):
+    s.in_ = InPort( nbits )
+    s.out = OutPort( nbits )
+
+  def elaborate_logic( s ):
+    @s.posedge_clk
+    def tick():
+      s.out.next = s.in_.value
+
+def test_RegisterOld():
+  register_tester( RegisterOld )
+
+#-------------------------------------------------------------------------
+# Register
+#-------------------------------------------------------------------------
+
+class Register( Model ):
+  def __init__( s, nbits ):
+    s.in_ = InPort( nbits )
+    s.out = OutPort( nbits )
+
+  def elaborate_logic( s ):
+    @s.posedge_clk
+    def tick():
+      s.out.n = s.in_
+
+def test_Register():
+  register_tester( Register )
--- new_pymtl/ValueNode.py
+++ new_pymtl/ValueNode.py
@@ -15,7 +15,7 @@ class ValueNode( object ):
 
   @v.setter
   def v( self, value ):
-    self.notify_sim()
+    self.notify_sim_comb_update()
     self.write( value )
 
   #-----------------------------------------------------------------------
@@ -24,14 +24,39 @@ class ValueNode( object ):
   @property
   def value( self ):
     return self
+  @value.setter
+  def value( self, value ):
+    self.notify_sim_comb_update()
+    self.write( value )
+
+  #-----------------------------------------------------------------------
+  # Write n property
+  #-----------------------------------------------------------------------
+  @property
+  def n( self ):
+    return self._shadow
+
+  @n.setter
+  def n( self, value ):
+    self.notify_sim_seq_update()
+    self._shadow_value = value
 
   #-----------------------------------------------------------------------
   # TEMPORARY: Backwards compatibility
   #-----------------------------------------------------------------------
-  @value.setter
-  def value( self, value ):
-    self.notify_sim()
-    self.write( value )
+  @property
+  def next( self ):
+    return self
+  @next.setter
+  def next( self, value ):
+    self.notify_sim_seq_update()
+    self._shadow_value = value
+
+  #-----------------------------------------------------------------------
+  # Flop
+  #-----------------------------------------------------------------------
+  def flop( self ):
+    self.write( self._shadow_value )
 
   #-----------------------------------------------------------------------
   # Write (Abstract)
@@ -43,13 +68,24 @@ class ValueNode( object ):
                                "implement the write() method!" )
 
   #-----------------------------------------------------------------------
-  # Notify Sim Hook (Abstract)
+  # Notify Sim of Combinational Update Hook (Abstract)
   #-----------------------------------------------------------------------
   # Another abstract method used as a hook by simulators tools.
   # Not meant to be implemented by subclasses.
   # NOTE: This approach uses closures, other methods can be found in:
   #       http://brg.csl.cornell.edu/wiki/lockhart-2013-03-18
   #       Is this the fastest approach?
-  def notify_sim( self ):
+  def notify_sim_comb_update( self ):
+    pass
+
+  #-----------------------------------------------------------------------
+  # Notify Sim of Sequential Update Hook (Abstract)
+  #-----------------------------------------------------------------------
+  # Another abstract method used as a hook by simulators tools.
+  # Not meant to be implemented by subclasses.
+  # NOTE: This approach uses closures, other methods can be found in:
+  #       http://brg.csl.cornell.edu/wiki/lockhart-2013-03-18
+  #       Is this the fastest approach?
+  def notify_sim_seq_update( self ):
     pass
 
