096afe53339a385473f285d92541a028ad2e8e60
Derek Lockhart <lockhart@csl.cornell.edu>
2013-04-05 15:11:22 -0400

    [new_gcd] Mux for gcd test (temp)

---
+++ new_gcd/Mux.py
@@ -0,0 +1,36 @@
+#=========================================================================
+# Muxes
+#=========================================================================
+# Asserting that a select value is out of range is not currently
+# translatable.
+
+from new_pymtl import *
+
+#-------------------------------------------------------------------------
+# Mux2
+#-------------------------------------------------------------------------
+
+class Mux( Model ):
+
+  @capture_args
+  def __init__( s, nbits = 1, nports = 2 ):
+
+    s.nports = nports
+    s.nsel   = get_sel_nbits( nports )
+
+    s.in_ = [ InPort( nbits ) for x in xrange( nports ) ]
+    s.sel = InPort  ( s.nsel )
+    s.out = OutPort ( nbits )
+
+  def elaborate_logic( s ):
+    @s.combinational
+    def comb_logic():
+      assert s.sel < s.nports
+
+      # TODO: fix to use PortLists !
+      s.out.v = s.in_[ s.sel.uint() ]
+
+  def line_trace( s ):
+    return "{:04x} {:04x} {:01x} () {:04x}" \
+      .format( s.in_[0].uint(), s.in_[1].uint(),
+               s.sel.uint(), s.out.uint() )
