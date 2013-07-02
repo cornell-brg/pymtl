b98c04abb0d7b1319b93b6ff7b941e158656d00d
Derek Lockhart <lockhart@csl.cornell.edu>
2013-07-01 23:01:53 -0400

    [new_pymtl] impl C++ queue with prealloc'd vector + ptrs

--- new_pymtl/EventQueue.cc
+++ new_pymtl/EventQueue.cc
@@ -3,32 +3,47 @@
 //========================================================================
 
 #include "EventQueue.h"
-#include <queue>
+#include <vector>
+#include <assert.h>
+#include <stdio.h>
 
 typedef void (*FuncPtr)( void );
 
-std::queue < FuncPtr > q;
+const unsigned int MAX = 1000;
+
+std::vector < FuncPtr > q ( MAX, NULL );
+unsigned int head  = 0;
+unsigned int tail  = 0;
+unsigned int count = 0;
 
 void init()
 {
   // http://stackoverflow.com/a/709161
-  std::queue < FuncPtr > empty;
+  std::vector < FuncPtr > empty ( MAX, NULL );
   std::swap( q, empty );
+  head  = 0;
+  tail  = 0;
+  count = 0;
 }
 
 void enq( FuncPtr func_ptr )
 {
-  q.push( func_ptr );
+  assert( count < MAX );
+  q[tail] = func_ptr;
+  tail = (tail + 1) % MAX;
+  count++;
 }
 
 FuncPtr deq( void )
 {
-  FuncPtr item = q.front();
-  q.pop();
+  assert( count > 0 );
+  FuncPtr item = q[head];
+  head = (head + 1) % MAX;
+  count--;
   return item;
 }
 
 int len( void )
 {
-  return q.size();
+  return count;
 }
