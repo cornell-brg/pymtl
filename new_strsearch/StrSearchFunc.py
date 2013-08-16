b5eb0b11932b69e2c16c568e3efceddab77cb093
Derek Lockhart <lockhart@csl.cornell.edu>
2013-08-16 13:31:39 -0400

    [new_strsearch] try using SigValue wrappers

--- new_strsearch/StrSearchFunc.py
+++ new_strsearch/StrSearchFunc.py
@@ -3,6 +3,27 @@
 #=========================================================================
 
 from new_pymtl import *
+from new_pymtl.SignalValue import SignalValue
+
+class StrSignalValue( SignalValue, str ):
+  def __init__( self, str_="" ):
+    self._str  = str_
+    self.nbits = None
+  def write_value( self, value ):
+    try:    self._str = value._str
+    except: self._str = value
+  def write_next( self, value ):
+    try:    self._next._str = value._str
+    except: self._next._str = value
+  def __eq__( self, value ):
+    return self._str == value
+  def __ne__( self, value ):
+    return self._str != value
+  def __str__( self ):
+    return self._str[0:10].zfill( 10 )
+  def data( self ):
+    return self._str
+
 
 #-------------------------------------------------------------------------
 # StrSearchMath
@@ -16,7 +37,7 @@ class StrSearchMath( Model ):
   def __init__( s, max_doc_chars, string ):
     s.string = string
 
-    s.in_    = InPort ( 8 * max_doc_chars )
+    s.in_    = InPort ( StrSignalValue() )
     s.out    = OutPort( 1 )
 
   #-----------------------------------------------------------------------
@@ -26,15 +47,68 @@ class StrSearchMath( Model ):
 
     @s.tick
     def find_logic():
-      to_chr = lambda i : chr( s.in_[i:i+8] )
-      doc = ''.join( [ to_chr( i ) for i in range( 0, 64*8, 8 )
-                    if to_chr( i ) != '\x00'] )
-      #print doc in s.string, doc, s.string
-      s.out.next = s.string in doc
+      s.out.next = s.string in s.in_.data()
 
-      #if s.in_ in s.string:
-      #  print "HELLO"
-      #  s.out.v = True
-      #else:
-      #  s.out.v = False
+
+#-------------------------------------------------------------------------
+# StrSearchAlg
+#-------------------------------------------------------------------------
+# Simple python string searcher.
+class StrSearchAlg( Model ):
+
+  #-----------------------------------------------------------------------
+  # __init__
+  #-----------------------------------------------------------------------
+  def __init__( s, max_doc_chars, string ):
+    s.string = string
+
+    s.in_    = InPort ( StrSignalValue() )
+    s.out    = OutPort( 1 )
+
+    s.DFA    = DFA = (len(string) + 1) * [0]
+
+    # build the deterministic finite-state automaton
+    i = 0
+    DFA[0] = -1
+    while i < len(string):
+      DFA[i+1] = DFA[i] + 1
+      while (DFA[i+1] > 0 and string[i] != string[DFA[i+1] - 1]):
+        DFA[i+1] = DFA[DFA[i+1] - 1] + 1
+      i+=1
+
+  #-----------------------------------------------------------------------
+  # find
+  #-----------------------------------------------------------------------
+  def elaborate_logic( s ):
+
+    @s.tick
+    def find_logic():
+
+      doc   = s.in_.data()
+      index = 0
+      j     = 0
+
+      while j < len(doc):
+
+        # if character not a match, rewind the DFA
+        #while (index > 0 and doc[j] != re[index]):
+        if (index > 0 and doc[j] != s.string[index]):
+          index = s.DFA[index]
+
+        # check if this character matches our current search
+        if (doc[j] == s.string[index]):
+          index += 1
+
+          # we found a match! now check for overlapping matches
+          if index == (len(s.string)):
+            index = s.DFA[index]
+            #print ("FOUND: "+doc[:j+1-len(s.string)]+"|"
+            #      +doc[j+1-len(s.string):j+1]+"|"+doc[j+1:])
+            s.out.next = True
+            return
+
+        # increment the doc indice
+        j += 1
+
+      s.out.next = False
 
--- new_strsearch/StrSearchFunc_test.py
+++ new_strsearch/StrSearchFunc_test.py
@@ -8,15 +8,6 @@ from new_pymtl        import *
 from StrSearchOO_test import strings, docs, reference
 
 #-------------------------------------------------------------------------
-# str_to_bits
-#-------------------------------------------------------------------------
-def str_to_bits( string, nchars ):
-  bits = Bits( nchars*8 )
-  for i, char in enumerate( bytearray( string ) ):
-    bits[i*8:i*8+8] = char
-  return bits
-
-#-------------------------------------------------------------------------
 # run_test
 #-------------------------------------------------------------------------
 def run_test( SearchModel ):
@@ -30,13 +21,12 @@ def run_test( SearchModel ):
     sim   = SimulationTool( model )
 
     for doc in docs:
-      doc_bits = str_to_bits( doc, 64 )
-      model.in_.v = doc_bits
+      model.in_.v = doc
       sim.cycle()
       assert model.out == reference[ i ]
       i += 1
 
-from StrSearchFunc import StrSearchMath #, StrSearchAlg
+from StrSearchFunc import StrSearchMath, StrSearchAlg
 
 #-------------------------------------------------------------------------
 # test_strsearch_math
@@ -47,5 +37,5 @@ def test_strsearch_math():
 #-------------------------------------------------------------------------
 # test_strsearch_alg
 #-------------------------------------------------------------------------
-#def test_strsearch_alg():
-#  run_test( StrSearchAlg )
+def test_strsearch_alg():
+  run_test( StrSearchAlg )
