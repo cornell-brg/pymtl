2bd2d31e73cc0f1b94faa1d254f92be85296aa84
Derek Lockhart <lockhart@csl.cornell.edu>
2013-08-16 13:51:59 -0400

    [new_strsearch] add impl with valrdy interface

---
+++ new_strsearch/StrSearchAB.py
@@ -0,0 +1,105 @@
+#=========================================================================
+# StrSearchFunc.py
+#=========================================================================
+
+from new_pymtl     import *
+from new_pmlib     import InValRdyBundle, OutValRdyBundle
+from StrSearchFunc import StrSignalValue
+
+#-------------------------------------------------------------------------
+# StrSearchMath
+#-------------------------------------------------------------------------
+# Simple python string searcher.
+class StrSearchMath( Model ):
+
+  #-----------------------------------------------------------------------
+  # __init__
+  #-----------------------------------------------------------------------
+  def __init__( s, max_doc_chars, string ):
+    s.string = string
+
+    s.in_    = InValRdyBundle( StrSignalValue() )
+    s.out    = OutValRdyBundle( 1 )
+
+  #-----------------------------------------------------------------------
+  # find
+  #-----------------------------------------------------------------------
+  def elaborate_logic( s ):
+
+    s.buf_full = False
+
+    @s.tick
+    def find_logic():
+      if s.out.val and s.out.rdy:
+        s.buf_full = False
+
+      if s.in_.val and s.in_.rdy:
+        s.out.msg.next = s.string in s.in_.msg.data()
+        s.buf_full = True
+
+      s.out.val.next = s.buf_full
+      s.in_.rdy.next = True
+
+
+#-------------------------------------------------------------------------
+# StrSearchAlg
+#-------------------------------------------------------------------------
+# Simple python string searcher.
+class StrSearchAlg( Model ):
+
+  #-----------------------------------------------------------------------
+  # __init__
+  #-----------------------------------------------------------------------
+  def __init__( s, max_doc_chars, string ):
+    s.string = string
+
+    s.in_    = InPort ( StrSignalValue() )
+    s.out    = OutPort( 1 )
+
+    s.DFA    = DFA = (len(string) + 1) * [0]
+
+    # build the deterministic finite-state automaton
+    i = 0
+    DFA[0] = -1
+    while i < len(string):
+      DFA[i+1] = DFA[i] + 1
+      while (DFA[i+1] > 0 and string[i] != string[DFA[i+1] - 1]):
+        DFA[i+1] = DFA[DFA[i+1] - 1] + 1
+      i+=1
+
+  #-----------------------------------------------------------------------
+  # find
+  #-----------------------------------------------------------------------
+  def elaborate_logic( s ):
+
+    @s.tick
+    def find_logic():
+
+      doc   = s.in_.data()
+      index = 0
+      j     = 0
+
+      while j < len(doc):
+
+        # if character not a match, rewind the DFA
+        #while (index > 0 and doc[j] != re[index]):
+        if (index > 0 and doc[j] != s.string[index]):
+          index = s.DFA[index]
+
+        # check if this character matches our current search
+        if (doc[j] == s.string[index]):
+          index += 1
+
+          # we found a match! now check for overlapping matches
+          if index == (len(s.string)):
+            index = s.DFA[index]
+            #print ("FOUND: "+doc[:j+1-len(s.string)]+"|"
+            #      +doc[j+1-len(s.string):j+1]+"|"+doc[j+1:])
+            s.out.next = True
+            return
+
+        # increment the doc indice
+        j += 1
+
+      s.out.next = False
+
---
+++ new_strsearch/StrSearchAB_test.py
@@ -0,0 +1,92 @@
+#=========================================================================
+# StrSearchFunc_test.py
+#=========================================================================
+#
+# PyMTL Functional Model of strsearch.
+
+from new_pymtl        import *
+from new_pmlib        import TestSource, TestSink
+from StrSearchOO_test import strings, docs, reference
+from StrSearchAB      import StrSignalValue
+
+#-------------------------------------------------------------------------
+# TestHarness
+#-------------------------------------------------------------------------
+
+class TestHarness( Model ):
+
+  def __init__( s, ModelType, string, src_msgs, sink_msgs,
+                src_delay, sink_delay ):
+
+    # Instantiate models
+
+    s.src  = TestSource ( StrSignalValue(), src_msgs,  src_delay  )
+    s.sort = ModelType  ( 64, string )
+    s.sink = TestSink   ( 1, sink_msgs, sink_delay )
+
+  def elaborate_logic( s ):
+
+    # Connect
+
+    s.connect( s.src.out,  s.sort.in_ )
+    s.connect( s.sort.out, s.sink.in_ )
+
+  def done( s ):
+    return s.src.done and s.sink.done
+
+  def line_trace( s ):
+    return s.src. line_trace() + " > " + \
+           s.sort.line_trace() + " > " + \
+           s.sink.line_trace()
+
+
+#-------------------------------------------------------------------------
+# run_test
+#-------------------------------------------------------------------------
+def run_test( ModelType, str_id, src_delay, sink_delay ):
+
+  # Test messages
+
+  ndocs     = len( docs )
+  src_msgs  = docs
+  sink_msgs = reference[str_id*ndocs:str_id*ndocs+ndocs]
+  string    = strings[ str_id ]
+
+  # Instantiate and elaborate the model
+
+  model = TestHarness( ModelType, string, src_msgs, sink_msgs,
+                       src_delay, sink_delay )
+  model.elaborate()
+
+  # Create a simulator using the simulation tool
+
+  sim = SimulationTool( model )
+
+  # Run the simulation
+
+  print ""
+
+  sim.reset()
+  while not model.done():
+    sim.print_line_trace()
+    sim.cycle()
+
+  # Add a couple extra ticks so that the VCD dump is nicer
+
+  sim.cycle()
+  sim.cycle()
+  sim.cycle()
+
+from StrSearchAB import StrSearchMath, StrSearchAlg
+
+#-------------------------------------------------------------------------
+# test_strsearch_math
+#-------------------------------------------------------------------------
+def test_strsearch_math():
+  run_test( StrSearchMath, 0, 0, 0 )
+
+#-------------------------------------------------------------------------
+# test_strsearch_alg
+#-------------------------------------------------------------------------
+#def test_strsearch_alg():
+#  run_test( StrSearchAlg )
