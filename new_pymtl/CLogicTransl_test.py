b9f2ee3125a2df8a31f7164d90941b587e06a015
Derek Lockhart <lockhart@csl.cornell.edu>
2013-11-08 03:34:01 -0500

    [new_pymtl] C transl, structural tests

--- new_pymtl/CLogicTransl_test.py
+++ new_pymtl/CLogicTransl_test.py
@@ -2,8 +2,8 @@
 # VerilogLogicTransl_test.py
 #=========================================================================
 
-import SimulationTool_seq_test  as sequential
-import SimulationTool_comb_test as combinational
+import SimulationTool_seq_test    as sequential
+import SimulationTool_struct_test as structural
 
 from CLogicTransl import CLogicTransl
 from subprocess   import check_output, STDOUT, CalledProcessError
@@ -126,7 +126,6 @@ def test_RegisterReset():
   sim.reset()
   assert model.out == 0
 
-
 # TODO: slices not supported
 # TODO: bit math doesn't work properly
 
@@ -136,10 +135,14 @@ def test_RegisterReset():
 #def test_SliceTempWriteCheck():
 #  translate( sequential.SliceTempWriteCheck( 16 ) )
 
+def test_MultipleWrites():
+  sim = translate( sequential.MultipleWrites() )
+  model = sim
 
-#def test_MultipleWrites():
-#  translate( sequential.MultipleWrites() )
-#
+  assert model.out == 0
+  sim.cycle(); assert model.out == 1
+  sim.cycle(); assert model.out == 1
+  sim.cycle(); assert model.out == 1
 
 #-------------------------------------------------------------------------
 # Complex Objects
@@ -147,64 +150,56 @@ def test_RegisterReset():
 #def test_Register_Obj():
 #  translate( sequential.Register( NO ) )
 
-##-------------------------------------------------------------------------
-## Combinational Logic
-##-------------------------------------------------------------------------
-#def test_PassThroughOld():
-#  translate( combinational.PassThroughOld(8) )
-#def test_PassThroughBits():
-#  translate( combinational.PassThroughBits(8) )
-#def test_PassThrough():
-#  translate( combinational.PassThrough(8) )
-#def test_FullAdder():
-#  translate( combinational.FullAdder() )
-#def test_RippleCarryAdderNoSlice():
-#  translate( combinational.RippleCarryAdderNoSlice( 4 ) )
-#def test_RippleCarryAdder():
-#  translate( combinational.RippleCarryAdder( 4 ) )
-#def test_SimpleBitBlast_8_to_8x1():
-#  translate( combinational.SimpleBitBlast( 8 ) )
-#def test_SimpleBitBlast_16_to_16x1():
-#  translate( combinational.SimpleBitBlast( 16 ) )
-#def test_ComplexBitBlast_8_to_8x1():
-#  translate( combinational.ComplexBitBlast( 8, 1 ) )
-#def test_ComplexBitBlast_8_to_4x2():
-#  translate( combinational.ComplexBitBlast( 8, 2 ) )
-#def test_ComplexBitBlast_8_to_2x4():
-#  translate( combinational.ComplexBitBlast( 8, 4 ) )
-#def test_ComplexBitBlast_8_to_1x8():
-#  translate( combinational.ComplexBitBlast( 8, 8 ) )
-#def test_SimpleBitMerge_8x1_to_8():
-#  translate( combinational.SimpleBitMerge( 16 ) )
-#def test_SimpleBitMerge_16x1_to_16():
-#  translate( combinational.SimpleBitMerge( 16 ) )
-#def test_ComplexBitMerge_8x1_to_8():
-#  translate( combinational.ComplexBitMerge( 8, 1 ) )
-#def test_ComplexBitMerge_4x2_to_8():
-#  translate( combinational.ComplexBitMerge( 8, 2 ) )
-#def test_ComplexBitMerge_2x4_to_8():
-#  translate( combinational.ComplexBitMerge( 8, 4 ) )
-#def test_ComplexBitMerge_1x8_to_8():
-#  translate( combinational.ComplexBitMerge( 8, 8 ) )
-#def test_SelfPassThrough():
-#  translate( combinational.SelfPassThrough( 2 ) )
-#def test_Mux():
-#  translate( combinational.Mux( 8, 4 ) )
-#def test_IfMux():
-#  translate( combinational.IfMux( 8 ) )
-#def test_CombSlicePassThrough():
-#  translate( combinational.CombSlicePassThrough() )
-#def test_CombSlicePassThroughWire():
-#  translate( combinational.CombSlicePassThroughWire() )
-#def test_CombSlicePassThroughStruct():
-#  translate( combinational.CombSlicePassThroughStruct() )
-#def test_BitMergePassThrough():
-#  translate( combinational.BitMergePassThrough() )
-#def test_ValueWriteCheck():
-#  translate( combinational.ValueWriteCheck( 16 ) )
-#def test_SliceWriteCheck():
-#  translate( combinational.SliceWriteCheck( 16 ) )
-#def test_SliceTempWriteCheck():
-#  translate( combinational.SliceTempWriteCheck( 16 ) )
-#def test_MultipleWrites():
-#  translate( combinational.MultipleWrites( 4 ) )
+#-------------------------------------------------------------------------
+# Structural
+#-------------------------------------------------------------------------
+
+def passthrough_tester( model ):
+  model.in_ = 8
+  # Note: no need to call cycle, no @combinational block
+  assert model.out   == 8
+  model.in_ = 9
+  model.in_ = 10
+  assert model.out == 10
+
+def test_PassThrough():
+  passthrough_tester( translate( structural.PassThrough(8) ) )
+
+def test_PassThroughWrapped():
+  passthrough_tester( translate( structural.PassThroughWrapped(8) ) )
+
+def test_PassThroughWrappedChain():
+  passthrough_tester( translate( structural.PassThroughWrappedChain(8) ) )
+
+def splitter_tester( model ):
+  model.in_ = 8
+  assert model.out0 == 8
+  assert model.out1 == 8
+  model.in_ = 9
+  model.in_ = 10
+  assert model.out0 == 10
+  assert model.out1 == 10
+
+def test_Splitter():
+  splitter_tester( translate(structural.Splitter(16)) )
+
+def test_SplitterWires():
+  splitter_tester( translate(structural.SplitterWires(16)) )
+
+def test_SplitterWrapped():
+  splitter_tester( translate(structural.SplitterWrapped(16)) )
+
+def test_SplitterPT_1():
+  splitter_tester( translate(structural.SplitterPT_1(16)) )
+
+def test_SplitterPT_2():
+  splitter_tester( translate(structural.SplitterPT_2(16)) )
+
+def test_SplitterPT_3():
+  splitter_tester( translate(structural.SplitterPT_3(16)) )
+
+def test_SplitterPT_4():
+  splitter_tester( translate(structural.SplitterPT_4(16)) )
+
+def test_SplitterPT_5():
+  splitter_tester( translate(structural.SplitterPT_5(16)) )
