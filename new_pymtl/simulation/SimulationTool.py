1486ff59e4908cf93c54ca4a19549ae0bb64e050
Derek Lockhart <lockhart@csl.cornell.edu>
2014-04-09 17:22:49 -0400

    [simulation] more refactoring of SimulationTool into sim_utils

--- new_pymtl/simulation/SimulationTool.py
+++ new_pymtl/simulation/SimulationTool.py
@@ -10,14 +10,10 @@ import pprint
 import collections
 import inspect
 import warnings
+import sim_utils as sim
 
 from sys               import flags
-from ..SignalValue     import SignalValue
-from ..ast_helpers     import get_method_ast
-from ast_visitor       import DetectLoadsAndStores
 from SimulationMetrics import SimulationMetrics, DummyMetrics
-
-from sim_utils         import connections_to_nets
 
 #-----------------------------------------------------------------------
 # SimulationTool
@@ -43,9 +39,9 @@ class SimulationTool( object ):
     self.model                = model
     self.ncycles              = 0
 
+    self._event_queue         = EventQueue()
     self._sequential_blocks   = []
     self._register_queue      = []
-    self._event_queue         = EventQueue()
     self._current_func        = None
 
     self._nets                = None # TODO: remove me
@@ -74,11 +70,15 @@ class SimulationTool( object ):
 
     # Construct a simulator for the provided model.
 
-    nets, slice_connections = connections_to_nets( self.model )
-    self._insert_signal_values( nets )
-    self._register_decorated_functions( self.model )
-    self._create_slice_callbacks( slice_connections )
-    self._register_cffi_updates()
+    signals                 = sim.collect_signals( model )
+    nets, slice_connections = sim.signals_to_nets( signals )
+
+    sim.insert_signal_values( self, nets )
+
+    sim.register_decorated_functions( model, self._event_queue,
+                                      self._sequential_blocks )
+    sim.create_slice_callbacks( slice_connections, self._event_queue )
+    sim.register_cffi_updates( model )
 
     self._nets = nets
 
@@ -228,239 +228,6 @@ class SimulationTool( object ):
       self.metrics.incr_add_callbk()
       if func != self._current_func:
         self._event_queue.enq( func.cb, func.id )
-
-  #---------------------------------------------------------------------
-  # _insert_signal_values
-  #---------------------------------------------------------------------
-  # Transform each net into a single SignalValue object. Model attributes
-  # currently referencing Signal objects will be modified to reference
-  # the SignalValue object of their associated net instead.
-  def _insert_signal_values( self, nets ):
-
-    # Utility functions which create SignalValue callbacks.
-
-    #-------------------------------------------------------------------
-    # create_comb_update_cb
-    #-------------------------------------------------------------------
-    def create_comb_update_cb( sim, svalue ):
-      def notify_sim_comb_update():
-        sim.add_event( svalue )
-      return notify_sim_comb_update
-
-    #-------------------------------------------------------------------
-    # create_seq_update_cb
-    #-------------------------------------------------------------------
-    def create_seq_update_cb( sim, svalue ):
-      def notify_sim_seq_update():
-        sim._register_queue.append( svalue )
-      return notify_sim_seq_update
-
-    # Each grouping represents a single SignalValue object. Perform a swap
-    # so that all attributes currently pointing to Signal objects in this
-    # grouping instead point to the SignalValue.
-    for group in nets:
-
-      # Get an element out of the set and use it to determine the bitwidth
-      # of the net, needed to create a properly sized SignalValue object.
-      # TODO: no peek() so have to pop() then reinsert it! Another way?
-      # TODO: what about BitStructs?
-      temp = group.pop()
-      group.add( temp )
-
-      # TODO: should this be visible to sim?
-      svalue       = temp.msg_type()
-      svalue._next = temp.msg_type()
-
-      #svalue._DEBUG_signal_names = group
-
-      # Add a callback to the SignalValue to notify SimulationTool every
-      # time a sequential update occurs (.next is written).
-      # TODO: currently all signals get this, necessary?
-      svalue.notify_sim_seq_update = create_seq_update_cb ( self, svalue )
-
-      # Create a callback for the SignalValue to notify SimulationTool
-      # every time a combinational update occurs (.value is written).
-      # We just store the callback for now, only add it later if we detect
-      # that a combinational block is sensitive to us
-      svalue._ucb                  = create_comb_update_cb( self, svalue )
-
-      # Modify model attributes currently referencing Signal objects to
-      # reference SignalValue objects instead.
-      for x in group:
-        # Set the value of the SignalValue object if we encounter a
-        # constant (check for Constant object instead?)
-        if isinstance( x._signalvalue, int ):
-          svalue.write_value( x._signalvalue )
-          svalue.constant = True
-        # Otherwise swap the value
-        else:
-          # We need 'in locals()' because of the nested function above,
-          # see: http://stackoverflow.com/a/4484946
-          exec( "x.parent.{} = svalue".format( x.name ) ) in locals()
-
-        # Also give signals a pointer to the SignalValue object.
-        # (Needed for VCD tracing and slice logic generator).
-        x._signalvalue = svalue
-
-  #---------------------------------------------------------------------
-  # _register_decorated_functions
-  #---------------------------------------------------------------------
-  # Register all decorated @tick, @posedge_clk, and @combinational
-  # functions with the simulator.  Sequential logic blocks get called
-  # any time cycle() is called, combinational logic blocks are registered
-  # with SignalValue objects and get added to the event queue as values
-  # change.
-  def _register_decorated_functions( self, model ):
-
-    # Add all cycle driven functions
-    self._sequential_blocks.extend( model._tick_blocks )
-    self._sequential_blocks.extend( model._posedge_clk_blocks )
-
-    #-------------------------------------------------------------------
-    # name_to_object
-    #-------------------------------------------------------------------
-    # Utility function to turn attributes/names acquired from the ast
-    # into Python objects
-    # TODO: should never use eval... but this is easy
-    # TODO: how to handle when self is neither 's' nor 'self'?
-    # TODO: how to handle temps!
-    def name_to_object( name ):
-      # Temporarily creates the names 'self' and 's' in the current
-      # scope.  SUPER HACKY
-      self = s = model
-      # If slice or list, get name components previous to indexing
-      if '[?]' in name:
-        name, extra = name.split('[?]', 1)
-      # Try to return the Python object attached to the name. If the
-      # object is not a  SignalValue or a list, we can't add it to
-      # the sensitivity list.  Sometimes this is okay (eg. constants),
-      # but sometimes this indicates an error in the user's code, so
-      # display a warning.
-      # In the case of a list, we need to reconstruct the name of each
-      # item in the list so we can try to add it to the sensitivity
-      # list. Return a tuple containing the list object, the list name
-      # and the attribute string the appears after the list indexing.
-      try:
-        x = eval( name )
-        if   isinstance( x, SignalValue ): return x
-        elif isinstance( x, list        ): return ( x, name, extra )
-        else:                              raise NameError
-      except NameError:
-        warnings.warn( "Cannot add variable '{}' to sensitivity list."
-                       "".format( name ), Warning )
-        return None
-
-    #-------------------------------------------------------------------
-    # add_senses
-    #-------------------------------------------------------------------
-    # Utility function to recursively add signals/lists of signals to
-    # the sensitivity list.
-    def add_senses( name ):
-      obj = name_to_object( name )
-      # If name_to_object returned a tuple, this is a list inside of a
-      # for loop.  Iteratively go through each object in the list and
-      # recursively call add_senses on it.
-      if   isinstance( obj, tuple ):
-        obj_list, list_name, attr = obj
-        for i, o in enumerate( obj_list ):
-          obj_name = "{}[{}]{}".format( list_name, i, attr )
-          add_senses( obj_name )
-      # If this is a signal value, add it to the sensitivity list
-      elif isinstance( obj, SignalValue ):
-        model._newsenses[ func ].append( obj._target_bits )
-
-
-    # Get the sensitivity list of each event driven (combinational) block
-    # TODO: do before or after we swap value nodes?
-    for func in model._combinational_blocks:
-      tree, _ = get_method_ast( func )
-      loads, stores = DetectLoadsAndStores().enter( tree )
-      for name in loads:
-        add_senses( name )
-
-    # Iterate through all @combinational decorated function names we
-    # detected, retrieve their associated function pointer, then add
-    # entries for each item in the function's sensitivity list to
-    # svalue_callbacks
-    # TODO: merge this code with above to reduce mem of data structures?
-    for func_ptr, sensitivity_list in model._newsenses.items():
-      func_ptr.id = self._event_queue.get_id()
-      func_ptr.cb = func_ptr
-      self.metrics.reg_eval( func_ptr.cb )
-      for signal_value in sensitivity_list:
-
-        # Only add "notify_sim" funcs if @comb blocks are sensitive to us
-        signal_value.notify_sim_comb_update = signal_value._ucb
-
-        # Prime the simulation by putting all events on the event_queue
-        # This will make sure all nodes come out of reset in a consistent
-        # state. TODO: put this in reset() instead?
-        signal_value.register_callback( func_ptr )
-        self._event_queue.enq( func_ptr.cb, func_ptr.id )
-
-        #self._DEBUG_signal_cbs[ signal_value ].append( func_ptr )
-
-    # Recursively perform for submodules
-    for m in model.get_submodules():
-      self._register_decorated_functions( m )
-
-  #---------------------------------------------------------------------
-  # _create_slice_callbacks
-  #---------------------------------------------------------------------
-  # All ConnectionEdges that contain bit slicing need to be turned into
-  # combinational blocks.  This significantly simplifies the connection
-  # graph update logic.
-  def _create_slice_callbacks( self, slice_connects ):
-
-    # Utility function to create our callback
-    def create_slice_cb_closure( c ):
-      src       = c.src_node._signalvalue
-      dest      = c.dest_node._signalvalue
-      src_addr  = c.src_slice  if c.src_slice  != None else slice( None )
-      dest_bits = dest[ c.dest_slice ] if c.dest_slice != None else dest
-      def slice_cb():
-        # We need to slice the src each time.  This is because writing
-        # to a BitSlice will updates the Bits it was sliced from, but
-        # not vice versa.
-        dest_bits.v = src[ src_addr ]
-      return slice_cb
-
-    for c in slice_connects:
-      src = c.src_node._signalvalue
-      # If slice is connect to a Constant, don't create a callback.
-      # Just write the constant value now.
-      if isinstance( src, int ):
-        dest      = c.dest_node._signalvalue
-        dest_addr = c.dest_slice if c.dest_slice != None else slice( None )
-        dest[ dest_addr ].v = src
-      # If slice is connected to another Signal, create a callback
-      # and put it on the combinational event queue.
-      else:
-        func_ptr = create_slice_cb_closure( c )
-        signal_value = c.src_node._signalvalue
-        signal_value.register_slice( func_ptr )
-        func_ptr.id = self._event_queue.get_id()
-        func_ptr.cb = func_ptr
-        self._event_queue.enq( func_ptr.cb, func_ptr.id )
-        self.metrics.reg_eval( func_ptr.cb, is_slice = True )
-        #self._DEBUG_signal_cbs[ signal_value ].append( func_ptr )
-
-  #---------------------------------------------------------------------
-  # _register_cffi_updates
-  #---------------------------------------------------------------------
-  def _register_cffi_updates( self ):
-
-    def visit_models( m ):
-      if hasattr( m, '_cffi_update' ):
-        for port, func_ptr in m._cffi_update.items():
-          signal_value = port._signalvalue
-          signal_value.register_slice( func_ptr )
-      # TODO: make recursive
-      #for subm in m.get_submodules():
-      #  visit_models( subm )
-
-    visit_models( self.model )
-
 
 #-----------------------------------------------------------------------
 # EventQueue
--- new_pymtl/simulation/sim_utils.py
+++ new_pymtl/simulation/sim_utils.py
@@ -21,17 +21,15 @@ def collect_signals( model ):
   return signals
 
 #-----------------------------------------------------------------------
-# connections_to_nets
+# signals_to_nets
 #-----------------------------------------------------------------------
 # Generate nets describing structural connections in the model.  Each
 # net describes a set of Signal objects which have been interconnected,
 # either directly or indirectly, by calls to connect().
-def connections_to_nets( model ):
+def signals_to_nets( signals ):
 
   nets           = []
   slice_connects = set()
-
-  signals = collect_signals( model )
 
   #---------------------------------------------------------------------
   # valid_connection
@@ -80,4 +78,242 @@ def connections_to_nets( model ):
     nets.append( net )
 
   return nets, slice_connects
+
+#---------------------------------------------------------------------
+# insert_signal_values
+#---------------------------------------------------------------------
+# Transform each net into a single SignalValue object. Model attributes
+# currently referencing Signal objects will be modified to reference
+# the SignalValue object of their associated net instead.
+def insert_signal_values( sim, nets ):
+
+  # Utility functions which create SignalValue callbacks.
+
+  #-------------------------------------------------------------------
+  # create_comb_update_cb
+  #-------------------------------------------------------------------
+  def create_comb_update_cb( sim, svalue ):
+    def notify_sim_comb_update():
+      sim.add_event( svalue )
+    return notify_sim_comb_update
+
+  #-------------------------------------------------------------------
+  # create_seq_update_cb
+  #-------------------------------------------------------------------
+  def create_seq_update_cb( sim, svalue ):
+    def notify_sim_seq_update():
+      sim._register_queue.append( svalue )
+    return notify_sim_seq_update
+
+  # Each grouping represents a single SignalValue object. Perform a swap
+  # so that all attributes currently pointing to Signal objects in this
+  # grouping instead point to the SignalValue.
+  for group in nets:
+
+    # Get an element out of the set and use it to determine the bitwidth
+    # of the net, needed to create a properly sized SignalValue object.
+    # TODO: no peek() so have to pop() then reinsert it! Another way?
+    # TODO: what about BitStructs?
+    temp = group.pop()
+    group.add( temp )
+
+    # TODO: should this be visible to sim?
+    svalue       = temp.msg_type()
+    svalue._next = temp.msg_type()
+
+    #svalue._DEBUG_signal_names = group
+
+    # Add a callback to the SignalValue to notify SimulationTool every
+    # time a sequential update occurs (.next is written).
+    # TODO: currently all signals get this, necessary?
+    svalue.notify_sim_seq_update = create_seq_update_cb ( sim, svalue )
+
+    # Create a callback for the SignalValue to notify SimulationTool
+    # every time a combinational update occurs (.value is written).
+    # We just store the callback for now, only add it later if we detect
+    # that a combinational block is sensitive to us
+    svalue._ucb                  = create_comb_update_cb( sim, svalue )
+
+    # Modify model attributes currently referencing Signal objects to
+    # reference SignalValue objects instead.
+    for x in group:
+      # Set the value of the SignalValue object if we encounter a
+      # constant (check for Constant object instead?)
+      if isinstance( x._signalvalue, int ):
+        svalue.write_value( x._signalvalue )
+        svalue.constant = True
+      # Otherwise swap the value
+      else:
+        # We need 'in locals()' because of the nested function above,
+        # see: http://stackoverflow.com/a/4484946
+        exec( "x.parent.{} = svalue".format( x.name ) ) in locals()
+
+      # Also give signals a pointer to the SignalValue object.
+      # (Needed for VCD tracing and slice logic generator).
+      x._signalvalue = svalue
+
+#---------------------------------------------------------------------
+# register_decorated_functions
+#---------------------------------------------------------------------
+# Register all decorated @tick, @posedge_clk, and @combinational
+# functions with the simulator.  Sequential logic blocks get called
+# any time cycle() is called, combinational logic blocks are registered
+# with SignalValue objects and get added to the event queue as values
+# change.
+def register_decorated_functions( model, event_queue, sequential_blocks ):
+
+  # Add all cycle driven functions
+
+  sequential_blocks.extend( model._tick_blocks )
+  sequential_blocks.extend( model._posedge_clk_blocks )
+
+  # Get the sensitivity list of each event driven (combinational) block
+  # TODO: do before or after we swap value nodes?
+
+  for func in model._combinational_blocks:
+    tree, _ = get_method_ast( func )
+    loads, stores = DetectLoadsAndStores().enter( tree )
+    for name in loads:
+      _add_senses( func, model, name )
+
+  # Iterate through all @combinational decorated function names we
+  # detected, retrieve their associated function pointer, then add
+  # entries for each item in the function's sensitivity list to
+  # svalue_callbacks
+  # TODO: merge this code with above to reduce mem of data structures?
+
+  for func_ptr, sensitivity_list in model._newsenses.items():
+    func_ptr.id = event_queue.get_id()
+    func_ptr.cb = func_ptr
+    #self.metrics.reg_eval( func_ptr.cb )
+    for signal_value in sensitivity_list:
+
+      # Only add "notify_sim" funcs if @comb blocks are sensitive to us
+      signal_value.notify_sim_comb_update = signal_value._ucb
+
+      # Prime the simulation by putting all events on the event_queue
+      # This will make sure all nodes come out of reset in a consistent
+      # state. TODO: put this in reset() instead?
+      signal_value.register_callback( func_ptr )
+      event_queue.enq( func_ptr.cb, func_ptr.id )
+
+      #self._DEBUG_signal_cbs[ signal_value ].append( func_ptr )
+
+  # Recursively perform for submodules
+  for m in model.get_submodules():
+    register_decorated_functions( m, event_queue, sequential_blocks )
+
+#-----------------------------------------------------------------------
+# _add_senses
+#-----------------------------------------------------------------------
+# Utility function to recursively add signals/lists of signals to
+# the sensitivity list.
+def _add_senses( func, model, name ):
+  obj = _attr_name_to_object( model, name )
+  # If name_to_object returned a tuple, this is a list inside of a
+  # for loop.  Iteratively go through each object in the list and
+  # recursively call add_senses on it.
+  if   isinstance( obj, tuple ):
+    obj_list, list_name, attr = obj
+    for i, o in enumerate( obj_list ):
+      obj_name = "{}[{}]{}".format( list_name, i, attr )
+      _add_senses( func, model, obj_name )
+  # If this is a signal value, add it to the sensitivity list
+  elif isinstance( obj, SignalValue ):
+    model._newsenses[ func ].append( obj._target_bits )
+
+#-----------------------------------------------------------------------
+# _attr_name_to_object
+#-----------------------------------------------------------------------
+# Utility function to turn attributes/names acquired from the ast
+# into Python objects
+# TODO: should never use eval... but this is easy
+# TODO: how to handle when self is neither 's' nor 'self'?
+# TODO: how to handle temps!
+def _attr_name_to_object( model, name ):
+  # Temporarily creates the names 'self' and 's' in the current
+  # scope.  SUPER HACKY
+  self = s = model
+  # If slice or list, get name components previous to indexing
+  if '[?]' in name:
+    name, extra = name.split('[?]', 1)
+  # Try to return the Python object attached to the name. If the
+  # object is not a  SignalValue or a list, we can't add it to
+  # the sensitivity list.  Sometimes this is okay (eg. constants),
+  # but sometimes this indicates an error in the user's code, so
+  # display a warning.
+  # In the case of a list, we need to reconstruct the name of each
+  # item in the list so we can try to add it to the sensitivity
+  # list. Return a tuple containing the list object, the list name
+  # and the attribute string the appears after the list indexing.
+  try:
+    x = eval( name )
+    if   isinstance( x, SignalValue ): return x
+    elif isinstance( x, list        ): return ( x, name, extra )
+    else:                              raise NameError
+  except NameError:
+    warnings.warn( "Cannot add variable '{}' to sensitivity list."
+                   "".format( name ), Warning )
+    return None
+
+
+#-----------------------------------------------------------------------
+# create_slice_callbacks
+#-----------------------------------------------------------------------
+# All ConnectionEdges that contain bit slicing need to be turned into
+# combinational blocks.  This significantly simplifies the connection
+# graph update logic.
+def create_slice_callbacks( slice_connects, event_queue ):
+
+  for c in slice_connects:
+    src = c.src_node._signalvalue
+    # If slice is connect to a Constant, don't create a callback.
+    # Just write the constant value now.
+    if isinstance( src, int ):
+      dest      = c.dest_node._signalvalue
+      dest_addr = c.dest_slice if c.dest_slice != None else slice( None )
+      dest[ dest_addr ].v = src
+    # If slice is connected to another Signal, create a callback
+    # and put it on the combinational event queue.
+    else:
+      func_ptr     = _create_slice_cb_closure( c )
+      signal_value = c.src_node._signalvalue
+      signal_value.register_slice( func_ptr )
+      func_ptr.id = event_queue.get_id()
+      func_ptr.cb = func_ptr
+      event_queue.enq( func_ptr.cb, func_ptr.id )
+      #self.metrics.reg_eval( func_ptr.cb, is_slice = True )
+      #self._DEBUG_signal_cbs[ signal_value ].append( func_ptr )
+
+#-----------------------------------------------------------------------
+# _create_slice_cb_closure
+#-----------------------------------------------------------------------
+# Utility function to create our callback
+def _create_slice_cb_closure( c ):
+  src       = c.src_node._signalvalue
+  dest      = c.dest_node._signalvalue
+  src_addr  = c.src_slice  if c.src_slice  != None else slice( None )
+  dest_bits = dest[ c.dest_slice ] if c.dest_slice != None else dest
+  def slice_cb():
+    # We need to slice the src each time.  This is because writing
+    # to a BitSlice will updates the Bits it was sliced from, but
+    # not vice versa.
+    dest_bits.v = src[ src_addr ]
+  return slice_cb
+
+#-----------------------------------------------------------------------
+# register_cffi_updates
+#-----------------------------------------------------------------------
+def register_cffi_updates( model ):
+
+  def visit_models( m ):
+    if hasattr( m, '_cffi_update' ):
+      for port, func_ptr in m._cffi_update.items():
+        signal_value = port._signalvalue
+        signal_value.register_slice( func_ptr )
+    # TODO: make recursive
+    #for subm in m.get_submodules():
+    #  visit_models( subm )
+
+  visit_models( model )
 
