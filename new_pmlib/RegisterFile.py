722d5ce521021c7648b86be2ad630486f9b849bd
Derek Lockhart <lockhart@csl.cornell.edu>
2014-04-22 14:27:41 -0400

    [pmlib] add const_zero config option to RegisterFile

--- new_pmlib/RegisterFile.py
+++ new_pmlib/RegisterFile.py
@@ -1,18 +1,25 @@
-#=========================================================================
-# Register File
-#=========================================================================
+#=======================================================================
+# RegisterFile.py
+#=======================================================================
 
 from new_pymtl import *
 
-import math
-
+#=======================================================================
+# RegisterFile
+#=======================================================================
 class RegisterFile( Model ):
 
-  def __init__( s, nbits = 32, nregs = 32, rd_ports = 1 ):
+  #---------------------------------------------------------------------
+  # elaborate_logic()
+  #---------------------------------------------------------------------
+  def __init__( s, nbits=32, nregs=32, rd_ports=1, const_zero=False ):
 
-    s.rd_ports = rd_ports
-    s.nregs    = nregs
-    addr_bits  = int( math.ceil( math.log( nregs, 2 ) ) )
+    s.rd_ports   = rd_ports
+    s.nregs      = nregs
+    s.nbits      = nbits
+    s.const_zero = const_zero
+
+    addr_bits  = get_sel_nbits( nregs )
 
     s.rd_addr  = [ InPort( addr_bits ) for x in xrange(rd_ports) ]
     s.rd_data  = [ OutPort( nbits )    for x in xrange(rd_ports) ]
@@ -20,35 +27,49 @@ class RegisterFile( Model ):
     s.wr_data  = InPort( nbits )
     s.wr_en    = InPort( 1 )
 
-    s.nbits = nbits
-    s.nregs = nregs
-
+  #---------------------------------------------------------------------
+  # elaborate_logic()
+  #---------------------------------------------------------------------
   def elaborate_logic( s ):
 
     s.regs     = [ Wire( s.nbits ) for x in xrange( s.nregs ) ]
 
+    #-------------------------------------------------------------------
+    # Combinational read logic
+    #-------------------------------------------------------------------
     @s.combinational
     def comb_logic():
+
       for i in xrange( s.rd_ports ):
-        # TODO: complains if no uint, list indices must be integers.
-        #       How do we make this translatable?
-        #raddr = s.rd_addr[i]
-        #assert raddr < s.nregs
-        #s.rd_data[i].value = s.regs[ raddr ]
         assert s.rd_addr[i] < s.nregs
         s.rd_data[i].value = s.regs[ s.rd_addr[i] ]
 
-    @s.posedge_clk
-    def seq_logic():
-      if s.wr_en.value:
+    # Select write logic depending on if this register file should have
+    # a constant zero register or not!
 
-        # TODO: this won't simulate correctly when translated/verilated!!!
-        #       mismatch between Verilog and PyMTL sim semantics...
-        #waddr = s.wr_addr.value.uint
-        #assert waddr < s.nregs
-        #s.regs[ waddr ].next = s.wr_data.value
+    #-------------------------------------------------------------------
+    # Sequential write logic
+    #-------------------------------------------------------------------
+    if not s.const_zero:
+      @s.posedge_clk
+      def seq_logic():
+        if s.wr_en:
+          s.regs[ s.wr_addr ].next = s.wr_data
 
-        s.regs[ s.wr_addr ].next = s.wr_data
+    #-------------------------------------------------------------------
+    # Sequential write logic with constant zero
+    #-------------------------------------------------------------------
+    else:
+      @s.posedge_clk
+      def seq_logic_const_zero():
+        if s.wr_en and s.wr_addr != 0:
+          s.regs[ s.wr_addr ].next = s.wr_data
+
+    # TODO: this won't simulate correctly when translated/verilated!!!
+    #       mismatch between Verilog and PyMTL sim semantics...
+    #waddr = s.wr_addr.value.uint()
+    #assert waddr < s.nregs
+    #s.regs[ waddr ].next = s.wr_data.value
 
   def line_trace( s ):
     return [x.uint() for x in s.regs]
--- new_pmlib/RegisterFile_test.py
+++ new_pmlib/RegisterFile_test.py
@@ -103,3 +103,55 @@ def test_regfile_2R1W( dump_vcd, test_verilog ):
   if dump_vcd:
     sim.dump_vcd( "plab2-test_regfile_2R1W.vcd" )
   sim.run_test()
+
+#-------------------------------------------------------------------------
+# Test 1R1W Register File - Constant Zero
+#-------------------------------------------------------------------------
+def test_regfile_1R1W_const_zero( dump_vcd, test_verilog ):
+
+  # Test vectors
+
+  test_vectors = [
+    # rd_addr0  rd_data0  wr_en  wr_addr  wr_data
+    [       0,   0x0000,     0,       0,   0x0000 ],
+    [       1,   0x0000,     0,       1,   0x0008 ],
+    # Write followed by Read
+    [       3,   0x0000,     1,       2,   0x0005 ],
+    [       2,   0x0005,     0,       2,   0x0000 ],
+    # Simultaneous Write and Read
+    [       3,   0x0000,     1,       3,   0x0007 ],
+    [       3,   0x0007,     1,       7,   0x0090 ],
+    [       7,   0x0090,     1,       3,   0x0007 ],
+    # Write to zero
+    [       0,   0x0000,     1,       0,   0x0FFF ],
+    [       0,   0x0000,     1,       4,   0x0FFF ],
+    [       0,   0x0000,     0,       4,   0x0BBB ],
+    [       0,   0x0000,     0,       4,   0x0FFF ],
+    [       4,   0x0FFF,     0,       0,   0x0000 ],
+  ]
+
+  # Instantiate and elaborate the model
+
+  model = RegisterFile( nbits=16, nregs=8, rd_ports=1, const_zero=True )
+  if test_verilog:
+    model = get_verilated( model )
+  model.elaborate()
+
+  # Define functions mapping the test vector to ports in model
+
+  def tv_in( model, test_vector ):
+    model.rd_addr[0].value = test_vector[0]
+    model.wr_en.value      = test_vector[2]
+    model.wr_addr.value    = test_vector[3]
+    model.wr_data.value    = test_vector[4]
+
+  def tv_out( model, test_vector ):
+    #if test_vector[3] != '?':
+    assert model.rd_data[0].value == test_vector[1]
+
+  # Run the test
+
+  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
+  if dump_vcd:
+    sim.dump_vcd( "plab2-test_regfile_1R1W_const_zero.vcd" )
+  sim.run_test()
