a81578995a24498d50066e87cea81bac271387fd
Derek Lockhart <lockhart@csl.cornell.edu>
2014-10-07 21:38:08 -0400

    [dotprod] add new, simpler DotProductCL

---
+++ new_dotprod/DotProductCL.py
@@ -0,0 +1,79 @@
+#=========================================================================
+# MatrixVecFL
+#=========================================================================
+
+import greenlet
+
+from new_pymtl import *
+from new_pmlib import *
+from MatrixVec import MatrixVec
+
+import numpy
+
+from pmlib_extra      import GreenletWrapper,BytesMemPortProxy
+from new_pmlib.queues import ChildReqRespQueueAdapter, ParentReqRespQueueAdapter
+from pmlib_extra      import ListMemPortAdapter
+
+def mreq( a ):
+  return MemMsg( 32, 32 ).req.mk_msg( 0, a, 0, 0 )
+
+def gen_addresses( size, a, b ):
+  x = []
+  for i in range( size ):
+    x.append( a+i*4 )
+    x.append( b+i*4 )
+  return list(reversed(x))
+
+
+class DotProductCL( Model ):
+
+  def __init__( s, mem_ifc_types, cpu_ifc_types ):
+    s.cpu_ifc = ChildReqRespBundle ( cpu_ifc_types )
+    s.mem_ifc = ParentReqRespBundle( mem_ifc_types )
+
+    s.cpu     = ChildReqRespQueueAdapter ( s.cpu_ifc )
+    s.mem     = ParentReqRespQueueAdapter( s.mem_ifc )
+
+    s.go    = False
+    s.size  = 0
+    s.src0  = 0
+    s.src1  = 0
+    s.data  = []
+
+    @s.tick_fl
+    def logic():
+      s.cpu.xtick()
+      s.mem.xtick()
+
+      if s.go:
+
+        if s.addrs and not s.mem.req_q.full():
+          s.mem.push_req( mreq( s.addrs.pop() ) )
+        if not s.mem.resp_q.empty():
+          s.data.append( s.mem.get_resp() )
+
+        if len( s.data ) == s.size*2:
+          result = numpy.dot( s.data[0::2], s.data[1::2] )
+          s.cpu.push_resp( result )
+          s.go = False
+
+      elif not s.cpu.req_q.empty() and not s.cpu.resp_q.full():
+        req = s.cpu.get_req()
+        if req.ctrl_msg   == 1: s.size = req.data
+        elif req.ctrl_msg == 2: s.src0 = req.data
+        elif req.ctrl_msg == 3: s.src1 = req.data
+        elif req.ctrl_msg == 0:
+          s.addrs = gen_addresses( s.size, s.src0, s.src1 )
+          s.go    = True
+
+  #-----------------------------------------------------------------------
+  # line_trace
+  #-----------------------------------------------------------------------
+
+  def line_trace( s ):
+    return '{} {}'.format(
+        s.cpu.req_q.in_,
+        s.mem.req_q.out, )
+
+  def elaborate_logic( s ):
+    pass
---
+++ new_dotprod/DotProductCL_test.py
@@ -0,0 +1,187 @@
+#------------------------------------------------------------------------------
+#==============================================================================
+# MatrixVecLaneBL_test
+#==============================================================================
+
+from new_pymtl      import *
+from new_pmlib      import TestSource, TestMemory, mem_msgs
+#from DotProductFL   import DotProductFL as DotProduct
+from DotProductCL   import DotProductCL as DotProduct
+from new_pmlib      import MemMsg
+from new_pmlib      import CP2Msg
+
+import pytest
+
+#------------------------------------------------------------------------------
+# TestHarness
+#------------------------------------------------------------------------------
+class TestHarness( Model ):
+
+  def __init__( s, lane_id, nmul_stages, mem_delay, test_verilog ):
+
+
+    s.memreq_params  = mem_msgs.MemReqParams( 32, 32 )
+    s.memresp_params = mem_msgs.MemRespParams( 32 )
+
+    s.mem  = TestMemory( s.memreq_params, s.memresp_params, 1, mem_delay )
+    mem_ifc = MemMsg   ( 32, 32)
+    cpu_ifc = CP2Msg   ( 5, 32 )
+
+    s.lane = DotProduct(mem_ifc, cpu_ifc )
+
+    if test_verilog:
+      s.lane = get_verilated( s.lane )
+
+
+  def elaborate_logic( s ):
+    s.mem_req =  Wire( s.memreq_params.nbits  )
+    s.mem_resp = Wire( s.memresp_params.nbits )
+
+    @s.combinational
+    def connect_structs():
+      s.mem_req.value = s.lane.mem_ifc.req_msg
+      s.mem.reqs[0].msg.value = s.memreq_params.mk_req(
+        s.lane.mem_ifc.req_msg.type,
+        s.lane.mem_ifc.req_msg.addr,
+        s.lane.mem_ifc.req_msg.len,
+        s.lane.mem_ifc.req_msg.data)
+
+      s.mem_resp.value = s.mem.resps[0].msg
+      tup = s.memresp_params.unpck_resp(s.mem_resp)
+      s.lane.mem_ifc.resp_msg.type.value = tup[0]
+      s.lane.mem_ifc.resp_msg.len.value = tup[1]
+      s.lane.mem_ifc.resp_msg.data.value = tup[2]
+
+      s.mem.reqs[0].val.value = s.lane.mem_ifc.req_val
+      s.lane.mem_ifc.req_rdy.value = s.mem.reqs[0].rdy
+
+      s.mem.resps[0].rdy.value =  s.lane.mem_ifc.resp_rdy
+      s.lane.mem_ifc.resp_val.value = s.mem.resps[0].val
+
+
+
+  def done( s ):
+    return s.lane.cpu_ifc.resp_val
+
+  def line_trace( s ):
+    #return "{} -> {}".format( s.lane.line_trace(), s.mem.line_trace() )
+    return "{} -> {}".format( s.lane.line_trace(), s.mem.line_trace() )
+
+#------------------------------------------------------------------------------
+# run_mvmult_test
+#------------------------------------------------------------------------------
+def run_mvmult_test( dump_vcd, vcd_file_name, model, lane_id,
+                     src_matrix, src_vector, exp_value):
+
+  model.elaborate()
+
+  sim = SimulationTool( model )
+  if dump_vcd:
+    sim.dump_vcd( vcd_file_name )
+
+  # Load the memory
+
+  model.mem.load_memory( src_matrix )
+  model.mem.load_memory( src_vector )
+
+  # Run the simulation
+
+  sim.reset()
+
+  # Set the inputs
+  m_baseaddr = src_matrix [0]
+  v_baseaddr = src_vector [0]
+  size = len(src_vector[1]) / 4
+  go = True
+
+  sim.print_line_trace()
+  sim.cycle()
+  sim.cycle()
+  model.lane.cpu_ifc.req_val.next = 1
+  model.lane.cpu_ifc.req_msg.data.next = size
+  model.lane.cpu_ifc.req_msg.ctrl_msg.next = 1
+  #print model.lane.cpu_ifc.req_msg.addr
+
+  sim.print_line_trace()
+  sim.cycle()
+  sim.cycle()
+  model.lane.cpu_ifc.req_val.next = 1
+  model.lane.cpu_ifc.req_msg.data.next = m_baseaddr
+  model.lane.cpu_ifc.req_msg.ctrl_msg.next = 2
+  #print model.lane.cpu_ifc.req_msg.addr
+
+  sim.print_line_trace()
+  sim.cycle()
+  sim.cycle()
+  model.lane.cpu_ifc.req_val.next = 1
+  model.lane.cpu_ifc.req_msg.data.next = v_baseaddr
+  model.lane.cpu_ifc.req_msg.ctrl_msg.next = 3
+
+  sim.cycle()
+  sim.cycle()
+  model.lane.cpu_ifc.req_val.next = 1
+  model.lane.cpu_ifc.req_msg.data.next = True
+  model.lane.cpu_ifc.req_msg.ctrl_msg.next = 0
+  #print model.lane.cpu_ifc.req_msg.addr
+
+  while not model.done() and sim.ncycles < 80:
+    sim.print_line_trace()
+    sim.cycle()
+    model.lane.cpu_ifc.req_val.next = 0
+  sim.print_line_trace()
+  assert model.done()
+
+
+  assert exp_value == model.lane.cpu_ifc.resp_msg
+
+  # Add a couple extra ticks so that the VCD dump is nicer
+
+  sim.cycle()
+  sim.cycle()
+  sim.cycle()
+
+
+#------------------------------------------------------------------------------
+# mem_array_32bit
+#------------------------------------------------------------------------------
+# Utility function for creating arrays formatted for memory loading.
+from itertools import chain
+def mem_array_32bit( base_addr, data ):
+  return [base_addr,
+          list( chain.from_iterable([ [x,0,0,0] for x in data ] ))
+         ]
+
+#------------------------------------------------------------------------------
+# test_mvmult
+#------------------------------------------------------------------------------
+#  5 1 3   1    16
+#  1 1 1 . 2  =  6
+#  1 2 1   3     8
+#
+@pytest.mark.parametrize(
+  ('mem_delay','nmul_stages'), [(0,1),(0,4),(5,1),(5,4)]
+)
+def test_dotproduct( dump_vcd, test_verilog, mem_delay, nmul_stages ):
+  lane = 0
+  run_mvmult_test( dump_vcd, "DP.vcd",
+                   TestHarness( lane, nmul_stages, mem_delay, test_verilog ),
+                   lane,
+                   # NOTE: C++ has dummy data between rows when you have array**!
+                   mem_array_32bit(  0, [ 5, 1 ,3 ]),
+                   mem_array_32bit( 80, [ 1, 2, 3 ]),
+                   16,
+                 )
+
+@pytest.mark.parametrize(
+  ('mem_delay','nmul_stages'), [(0,1),(0,4),(5,1),(5,4)]
+)
+def test_2dotprod( dump_vcd, test_verilog, mem_delay, nmul_stages ):
+  lane = 0
+  run_mvmult_test( dump_vcd, "DP.vcd",
+                   TestHarness( lane, nmul_stages, mem_delay, test_verilog ),
+                   lane,
+                   # NOTE: C++ has dummy data between rows when you have array**!
+                   mem_array_32bit(  0, [ 5, 1 ,3, 9, 10 ]),
+                   mem_array_32bit( 80, [ 1, 2, 3, 9, 0 ]),
+                   16+81,
+                 )
