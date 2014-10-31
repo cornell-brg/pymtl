8764efb234550394b1b4ee94abc7b70ddad5e35c
Derek Lockhart <lockhart@csl.cornell.edu>
2014-10-31 18:22:58 -0400

    [scripts] rm legacy code, add rm_pycache utility

---
+++ scripts/rm_pycache.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+#========================================================================
+# rm_pycache.py
+#========================================================================
+# Utility script to remove all .pyc files and __pycache__ directories.
+
+import os
+
+def main():
+
+  for root, dirs, files in os.walk('.'):
+    pycaches = [os.path.join(root, x) for x in dirs if x == '__pycache__']
+    for i in pycaches:
+      print 'rm: {}'.format(i)
+      os.system( 'rm -r {}'.format(i) )
+
+  for root, dirs, files in os.walk('.'):
+    pycs = [os.path.join(root, x) for x in files if x.endswith('.pyc')]
+    for i in pycs:
+      print 'rm: {}'.format(i)
+      os.remove( i )
+
+if __name__ == "__main__":
+  main()
