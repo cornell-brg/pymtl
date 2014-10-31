0cbbf0d25cdde6ab4b2148ed063cda22b3756898
Derek Lockhart <lockhart@csl.cornell.edu>
2014-10-31 14:33:47 -0400

    [mem,pclib] fix tests broken by pclib reorg

--- pclib/rtl/__init__.py
+++ pclib/rtl/__init__.py
@@ -4,5 +4,13 @@ from Decoder      import Decoder
 from RegisterFile import RegisterFile
 from Crossbar     import Crossbar
 
+from queues_rtl import (
+  SingleElementNormalQueue,
+  SingleElementBypassQueue,
+  NormalQueue,
+  SingleElementPipelinedQueue,
+  SingleElementSkidQueue,
+)
+
 import arith
 
