0e691dcbfb16257a62de2c5d350067529729c252
Derek Lockhart <lockhart@csl.cornell.edu>
2015-01-21 16:12:53 -0500

    [build] add setup.py, fix requirements.txt

---
+++ setup.py
@@ -0,0 +1,50 @@
+# setup.py inspired by the PyPA sample project:
+# https://github.com/pypa/sampleproject/blob/master/setup.py
+
+from setuptools import setup, find_packages
+from codecs     import open   # To use a consistent encoding
+from os         import path
+
+def get_long_description():
+  here = path.abspath(path.dirname(__file__))
+  with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+
+    name    = 'pymtl',
+    version = '1.0alpha', # https://www.python.org/dev/peps/pep-0440/
+
+    description      = 'Python-based hardware modeling framework',
+    long_description = get_long_description(),
+    url              = 'https://github.com/cornell-brg/pymtl',
+
+    author       = 'Derek Lockhart',
+    author_email = 'lockhart@csl.cornell.edu',
+
+    # BSD 3-Clause License:
+    # - http://choosealicense.com/licenses/bsd-3-clause
+    # - http://opensource.org/licenses/BSD-3-Clause
+    license='BSD',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: BSD License',
+      'Programming Language :: Python :: 2.7',
+      'Operating System :: MacOS :: MacOS X',
+      'Operating System :: POSIX :: Linux',
+    ],
+
+    packages = find_packages(
+      exclude=['scripts', 'tests', 'ubmark', 'perf_tests']
+    ),
+
+    install_requires = [
+      'pytest',
+      'cffi',
+      'greenlet',
+      'numpy>=1.9.0',
+    ],
+
+)
