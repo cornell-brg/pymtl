1894db19e9c8aec30deeeab4616100aa1e45840d
Christopher Batten <cbatten@cornell.edu>
2015-02-07 09:56:38 -0500

    [pclib/ifcs] import valrdy_to_str into pclib.ifcs namespace

--- pclib/ifcs/__init__.py
+++ pclib/ifcs/__init__.py
@@ -5,5 +5,7 @@ from NetMsg              import NetMsg
 from MemMsg              import MemMsg
 from CP2Msg              import CP2Msg
 
+from valrdy              import valrdy_to_str
+
 import mem_msgs
-import valrdy
+
