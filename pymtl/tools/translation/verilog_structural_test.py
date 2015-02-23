14e38ac8654cdb90e4cc4dc828a4d0f140970db9
Derek Lockhart <lockhart@csl.cornell.edu>
2015-02-23 16:37:29 -0500

    [pymtl.model.trans] add test for verilog transl bugs

--- pymtl/tools/translation/verilog_structural_test.py
+++ pymtl/tools/translation/verilog_structural_test.py
@@ -3,6 +3,7 @@
 #=======================================================================
 
 import pytest
+import collections
 
 from verilator_sim import TranslationTool
 
@@ -53,6 +54,67 @@ pytestmark = requires_verilator
 ]]
 
 #-----------------------------------------------------------------------
+# verify_pipeline
+#-----------------------------------------------------------------------
+def verify_pipeline( Model ):
+  model = Model()
+  model.elaborate()
+  sim = SimulationTool( model )
+
+  pipeline = collections.deque( maxlen=3 )
+  pipeline.extend( [0]*3 )
+
+  for i in range( 10 ):
+    model.in_.value = i
+    pipeline.appendleft( i )
+    sim.cycle()
+    assert model.out == pipeline[-1]
+
+#-----------------------------------------------------------------------
+# MultipleParamTranslations
+#-----------------------------------------------------------------------
+def test_MultipleParamTranslations( setup_sim ):
+  class ParamClass( Model ):
+    def __init__( s, id_ ):
+      s.in_, s.out = InPort(4), OutPort(4)
+      s.id_        = id_
+      @s.posedge_clk
+      def logic():
+        s.out.next = s.in_
+  class ParentClass( Model ):
+    def __init__( s ):
+      s.in_, s.out = InPort(4), OutPort(4)
+      s.m = [ TranslationTool(ParamClass( x )) for x in range( 3 ) ]
+      s.connect( s.in_,      s.m[0].in_ )
+      s.connect( s.m[0].out, s.m[1].in_ )
+      s.connect( s.m[1].out, s.m[2].in_ )
+      s.connect( s.m[2].out, s.out      )
+
+  verify_pipeline( ParentClass )
+
+#-----------------------------------------------------------------------
+# MultipleNoParamTranslations
+#-----------------------------------------------------------------------
+@pytest.mark.xfail
+def test_MultipleNoParamTranslations( setup_sim ):
+  class NoParamClass( Model ):
+    def __init__( s ):
+      s.in_, s.out = InPort(4), OutPort(4)
+      @s.posedge_clk
+      def logic():
+        s.out.next = s.in_
+  class ParentClass( Model ):
+    def __init__( s ):
+      s.in_, s.out = InPort(4), OutPort(4)
+      s.m = [ TranslationTool( NoParamClass() ) for x in range( 3 ) ]
+      s.connect( s.in_,      s.m[0].in_ )
+      s.connect( s.m[0].out, s.m[1].in_ )
+      s.connect( s.m[1].out, s.m[2].in_ )
+      s.connect( s.m[2].out, s.out      )
+
+  verify_pipeline( ParentClass )
+
+#-----------------------------------------------------------------------
 # local_setup_sim
 #-----------------------------------------------------------------------
 # - (?) create a vcd dump of the simulation
@@ -61,5 +123,4 @@ pytestmark = requires_verilator
 # - create a simulator with the SimulationTool
 
 from verilog_behavioral_test import local_setup_sim
-
 
