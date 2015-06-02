066ff3bd3043559015d34b5739851981c0834ea9
Derek Lockhart <lockhart@csl.cornell.edu>
2015-06-02 14:38:44 -0400

    [pclib.rtl,proc] undo RegisterFile hack added in 73513fe

--- pclib/rtl/RegisterFile_test.py
+++ pclib/rtl/RegisterFile_test.py
@@ -49,7 +49,6 @@ def test_regfile_1R1W( dump_vcd, test_verilog ):
     model.wr_data.value    = test_vector[4]
 
   def tv_out( model, test_vector ):
-    #if test_vector[3] != '?':
     assert model.rd_data[0].value == test_vector[1]
 
   # Run the test
@@ -152,3 +151,54 @@ def test_regfile_1R1W_const_zero( dump_vcd, test_verilog ):
 
   sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
   sim.run_test()
+
+#-------------------------------------------------------------------------
+# Test 2R2W Register File
+#-------------------------------------------------------------------------
+def test_regfile_2R2W( dump_vcd, test_verilog ):
+
+  # Test vectors
+
+  test_vectors = [
+    # ---read 0---  ---read 1---  -----write 0-----  -----write 1-----
+    # addr    data  addr    data  en  addr     data  en  addr     data
+    [    0, 0x0000,    4, 0x0000,  0,    0,  0x0000,  0,    0,  0x0000],
+    [    3, 0x0000,    4, 0x0000,  1,    4,  0x0005,  0,    4,  0x0005],
+    [    2, 0x0000,    4, 0x0005,  0,    0,  0x0006,  1,    2,  0x0006],
+    [    2, 0x0006,    2, 0x0006,  0,    3,  0x0007,  0,    3,  0x0007],
+    [    7, 0x0000,    3, 0x0000,  0,    7,  0x0090,  1,    7,  0x0010],
+    [    4, 0x0005,    7, 0x0010,  0,    7,  0x0090,  0,    7,  0x0020],
+    [    6, 0x0000,    7, 0x0010,  1,    7,  0x0090,  1,    6,  0x0090],
+    [    6, 0x0090,    7, 0x0090,  1,    7,  0x0011,  0,    6,  0x0011],
+    [    6, 0x0090,    7, 0x0011,  0,    0,  0x0000,  0,    0,  0x0000],
+  ]
+
+  # Instantiate and elaborate the model
+
+  model = RegisterFile( nbits=16, nregs=8, rd_ports=2, wr_ports=2 )
+  model.vcd_file = dump_vcd
+  if test_verilog:
+    model = TranslationTool( model )
+  model.elaborate()
+
+  # Define functions mapping the test vector to ports in model
+
+  def tv_in( model, test_vector ):
+    model.rd_addr[0].value = test_vector[0]
+    model.rd_addr[1].value = test_vector[2]
+    model.wr_en  [0].value = test_vector[4]
+    model.wr_addr[0].value = test_vector[5]
+    model.wr_data[0].value = test_vector[6]
+    model.wr_en  [1].value = test_vector[7]
+    model.wr_addr[1].value = test_vector[8]
+    model.wr_data[1].value = test_vector[9]
+
+  def tv_out( model, test_vector ):
+    assert model.rd_data[0].value == test_vector[1]
+    assert model.rd_data[1].value == test_vector[3]
+
+  # Run the test
+
+  sim = TestVectorSimulator( model, test_vectors, tv_in, tv_out )
+  sim.run_test()
+
