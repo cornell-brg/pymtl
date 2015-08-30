2896bf3475c65937c1c1f96f4bce9fc0445e894a
Christopher Batten <cbatten@cornell.edu>
2015-08-30 16:58:17 -0400

    removing old models, tests, ubmarks

--- proc/parc_fl/__init__.py
+++
@@ -1,9 +0,0 @@
-#=========================================================================
-# Modular Python Build System __init__ file
-#=========================================================================
-
-# List of single-class modules
-
-from ParcProcFL    import ParcProcFL
-from GenericXcelFL import GenericXcelFL
-
