fecfdd354123891d5e17e84598b67b96a91ae745
Shunning Jiang <sj634@cornell.edu>
2016-10-16 21:19:09 -0400

    [pclib/rtl] Revamp RRArbiterEn

--- pclib/rtl/arbiters.py
+++ pclib/rtl/arbiters.py
@@ -81,35 +81,77 @@ class RoundRobinArbiter( Model ):
 class RoundRobinArbiterEn( Model ):
   '''Round Robin Arbiter model with an enable bit.
 
-  When the aribter enable bit is set high, the arbitration takes place
-  else the arbitration is disabled.
+  Only when 1) the arbiter enable bit is set high AND 2) there is some
+  valid arbitration, the priority register is updated. This breaks the
+  val-rdy dependency.
   '''
-
   def __init__( s, nreqs ):
 
-    s.en         = InPort  ( 1 )
-    s.reqs       = InPort  ( nreqs )
-    s.grants     = OutPort ( nreqs )
+    nreqsX2  = nreqs * 2
 
-    ARB    = 1
-    NO_ARB = 0
+    s.en     = InPort ( 1 )
+    s.reqs   = InPort ( nreqs )
+    s.grants = OutPort( nreqs )
 
-    # Request Mux
+    # priority enable
 
-    s.reqs_mux   = m = Mux( nreqs, nports=2 )
+    s.priority_en = Wire( 1 )
+
+    # priority register
+
+    s.priority_reg = m = RegEnRst( nreqs, reset_value = 1 )
+
     s.connect_dict({
-      m.in_[NO_ARB] : 0,
-      m.in_[ARB]    : s.reqs,
-      m.sel         : s.en
+      m.en           : s.priority_en,
+      m.in_[1:nreqs] : s.grants[0:nreqs-1],
+      m.in_[0]       : s.grants[nreqs-1]
     })
 
-    # round robin arbiter
+    s.kills        = Wire( 2*nreqs + 1 )
+    s.priority_int = Wire( 2*nreqs )
+    s.reqs_int     = Wire( 2*nreqs )
+    s.grants_int   = Wire( 2*nreqs )
 
-    s.rr_arbiter = RoundRobinArbiter( nreqs )
+    #-------------------------------------------------------------------
+    # comb_arbitrate
+    #-------------------------------------------------------------------
 
-    s.connect( s.rr_arbiter.reqs,   s.reqs_mux.out )
-    s.connect( s.rr_arbiter.grants, s.grants       )
+    @s.combinational
+    def comb_arbitrate():
+
+      s.kills[0].value = 1
+
+      s.priority_int[    0:nreqs  ].value = s.priority_reg.out
+      s.priority_int[nreqs:nreqsX2].value = 0
+      s.reqs_int    [    0:nreqs  ].value = s.reqs
+      s.reqs_int    [nreqs:nreqsX2].value = s.reqs
+
+      # Calculate the kill chain
+      for i in range( nreqsX2 ):
+
+        # Set internal grants
+        if s.priority_int[i].value:
+          s.grants_int[i].value = s.reqs_int[i]
+        else:
+          s.grants_int[i].value = ~s.kills[i] & s.reqs_int[i]
+
+        # Set kill signals
+        if s.priority_int[i].value:
+          s.kills[i+1].value = s.grants_int[i]
+        else:
+          s.kills[i+1].value = s.kills[i] | s.grants_int[i]
+
+      # Assign the output ports
+      for i in range( nreqs ):
+        s.grants[i].value = s.grants_int[i] | s.grants_int[nreqs+i]
+
+    #-------------------------------------------------------------------
+    # comb_feedback
+    #-------------------------------------------------------------------
+
+    @s.combinational
+    def comb_feedback():
+      s.priority_en.value = ( s.grants != 0 ) & s.en
 
   def line_trace( s ):
     return "{} | {}".format( s.reqs, s.grants )
-
